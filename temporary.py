9be361515971d865e20f8ce6bcb0fa77e1b1b5d2
Sebastiaan Zeeff <33516116+SebastiaanZ@users.noreply.github.com>
2020-01-13 10:54:50 +0100

    Add python version check

--- temporary.py
+++ temporary.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class FooBar:
     pass
 
@@ -8,3 +11,8 @@ except:
     pass
 
 a = 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567
+
+friend = "something"
+
+print(f"hello {friend}")
+print(sys.version)
