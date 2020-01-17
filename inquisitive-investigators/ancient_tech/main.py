1cf9b4a6afa8dc10c843f039d1324d82bbad81af
Monika <devindematto@gmail.com>
2020-01-17 12:22:47 -0500

    quick example of ids/references

--- inquisitive-investigators/ancient_tech/main.py
+++ inquisitive-investigators/ancient_tech/main.py
@@ -3,10 +3,17 @@ from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 
 
+# Float Layout Documentation
+# https://kivy.org/doc/stable/api-kivy.uix.floatlayout.html
 class Main(FloatLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Grabs reference of ID and calls text element
+        print(self.test.text)
 
 
+# App Documentation
+# https://kivy.org/doc/stable/api-kivy.app.html?highlight=app#module-kivy.app
 class AncientTechApp(App):
     def build(self):
         return Main()
