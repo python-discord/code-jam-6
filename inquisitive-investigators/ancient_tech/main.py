8519d81418e333f17e33b4d4c6740f2f953e121b
Monika <devindematto@gmail.com>
2020-01-17 12:07:22 -0500

    Added Base Structure and KV lang file

--- inquisitive-investigators/ancient_tech/main.py
+++ inquisitive-investigators/ancient_tech/main.py
@@ -1,1 +1,16 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+
+
+class Main(FloatLayout):
+    pass
+
+
+class AncientTechApp(App):
+    def build(self):
+        return Main()
+
+
+if __name__ == '__main__':
+    AncientTechApp().run()
