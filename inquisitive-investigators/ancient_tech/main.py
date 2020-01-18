e671f2510c39799fb7a78f3f4664c4c7ea0392bb
Monika <devindematto@gmail.com>
2020-01-17 19:11:14 -0500

    Created basic setup of File Explorer

--- inquisitive-investigators/ancient_tech/main.py
+++ inquisitive-investigators/ancient_tech/main.py
@@ -1,4 +1,7 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 
@@ -6,10 +9,32 @@ from kivy.uix.floatlayout import FloatLayout
 # Float Layout Documentation
 # https://kivy.org/doc/stable/api-kivy.uix.floatlayout.html
 class Main(FloatLayout):
+    pass
+
+
+# Box Layout Documentation
+# https://kivy.org/doc/stable/api-kivy.uix.boxlayout.html?highlight=box%20layout
+class FileMain(BoxLayout):
+    pass
+
+
+# Stack Layout Documentation
+# https://kivy.org/doc/stable/api-kivy.uix.stacklayout.html?highlight=layout#kivy.uix.stacklayout.StackLayout.spacing
+class FileBrowser(StackLayout):
+    pass
+
+
+# Label Will have to change to own Widget and take in fields such as Date, Time, File Size, etc...
+# https://kivy.org/doc/stable/api-kivy.uix.label.html?highlight=label
+class File(Label):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # Grabs reference of ID and calls text element
-        print(self.test.text)
+
+
+# Box Layout Documentation
+# https://kivy.org/doc/stable/api-kivy.uix.boxlayout.html?highlight=box%20layout
+class Footer(BoxLayout):
+    pass
 
 
 # App Documentation
