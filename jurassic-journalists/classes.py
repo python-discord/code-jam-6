e1497358f4117778bc521c186455a4aac602ea51
Greyson Jennings <tallesttower1@gmail.com>
2020-01-18 21:42:39 +0100

    created proof of concept rendering loop. Not to be used as a module, yet.

--- jurassic-journalists/classes.py
+++ jurassic-journalists/classes.py
@@ -1,11 +1,11 @@
 #classes.py
-
+from PIL import ImageFont
 #letter class- each letter is one of these objects, and is rendered in order.
 class Letter:
-    def __init__(char,size,font,color = (0,0,0,1),b=False,i=False,u=False):
+    def __init__(self,char,size,font,color = (0,0,0,1),b=False,i=False,u=False):
         self.char = char
         self.size = size
-        self.font = font
+        self.font = ImageFont.truetype(font, size)
         self.color = color
         self.b = b
         self.i = i
@@ -13,14 +13,17 @@ class Letter:
         #Map:
         #char: character.
         #size: size of letter.
-        #font: reference to font.
+        #font: PIL truetype font object. TODO: add handling for other types
         #color: color of letter, RGBA tuple, range 0-1.
         #b: Bold flag.
         #i: Italics flag.
         #u: Underlined flag.
+    def getKerning(self):
+        #gets dimensions as tuple(w,h) that it will be when rendered.
+        return self.font.getsize(self.char)
         
 
 #Witeout class- the little squiggle you have to do to erase words.
 class WiteOut:
-    def __init__():
+    def __init__(self):
         pass
