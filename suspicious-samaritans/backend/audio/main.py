7dd72db2e3975335e1fff57ea3687b760246da3b
Turreted <gideonmitchell01@gmail.com>
2020-01-17 19:36:35 -0600

    adds basic structure and audio

---
+++ suspicious-samaritans/backend/audio/main.py
@@ -0,0 +1,19 @@
+from pydub import AudioSegment
+from pydub import playback
+import numpy as np
+import os
+
+
+audiofile = os.path.abspath('./samples/drums.wav')
+extension = audiofile[audiofile.rfind('.') + 1:]
+
+clip = AudioSegment.from_file(audiofile, format=extension)
+# raw_bytes = clip.raw_data
+total_frames =  clip.frame_count()
+
+#MODIFY THIS
+samples = clip.get_array_of_samples()
+
+
+playback.play(clip)
+
