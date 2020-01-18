95b601e4635d982e7734050b43203d0c9b661c66
biskette <justin.stone@moduleit.co.uk>
2020-01-18 13:59:41 +0000

    Created the fundamentals of a rotating object.

--- gutsy-gamblers/main.py
+++ gutsy-gamblers/main.py
@@ -1,11 +1,41 @@
 import kivy
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import NumericProperty
+from kivy.animation import Animation
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
 kivy.require('1.11.1')
+
+
+# Widget element things #
+class RotatingWidget(FloatLayout):
+    angle = NumericProperty(0)
+
+    def __init__(self, **kwargs):
+        super(RotatingWidget, self).__init__(**kwargs)
+
+        # Duration is how long it takes to do a full 360 in seconds.
+        anim = Animation(angle=-360, duration=2)
+        anim += Animation(angle=-360, duration=2)
+        anim.repeat = True
+        anim.start(self)
+
+    def on_angle(self, item, angle):
+        if angle == -360:
+            item.angle = 0
+
+
+# Screens in the App #
+class MainScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        self.add_widget(RotatingWidget())
+
+    def settings_button(self):
+        SettingsScreen()
 
 
 class SettingsScreen(Popup):
@@ -15,11 +45,6 @@ class SettingsScreen(Popup):
         settings_popup = Popup(title="Settings", content=Label(text='Settings window!'),
                                size_hint=(None, None), size=(200, 200))
         settings_popup.open()
-
-
-class MainScreen(Screen):
-    def settings_button(self):
-        SettingsScreen()
 
 
 class WindowManager(ScreenManager):
