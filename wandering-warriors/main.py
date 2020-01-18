c55bc3b5e5d05ff79578d272bd30f4d521898238
Ryan Gables <regables@gmail.com>
2020-01-18 11:40:23 -0800

    initial kivy app

---
+++ wandering-warriors/main.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+
+class Abacus(Widget):
+    pass
+
+
+class DrawingInput(Widget):
+    pass
+
+
+class Ledger(Widget):
+    pass
+
+
+class AbacusApp(App):
+    def build(self):
+        Builder.load_file("abacus.kv")
+        app = Abacus()
+        return app
+
+
+if __name__ == "__main__":
+    AbacusApp().run()
