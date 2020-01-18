387bc7cc5bd27fa85a4dad7296e28b9c28f1e883
Monika <devindematto@gmail.com>
2020-01-17 19:53:01 -0500

    Updated Browser

--- inquisitive-investigators/ancient_tech/main.py
+++ inquisitive-investigators/ancient_tech/main.py
@@ -1,44 +1,40 @@
+from pathlib import Path
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.uix.stacklayout import StackLayout
-from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 
 
-# Float Layout Documentation
-# https://kivy.org/doc/stable/api-kivy.uix.floatlayout.html
 class Main(FloatLayout):
     pass
 
 
-# Box Layout Documentation
-# https://kivy.org/doc/stable/api-kivy.uix.boxlayout.html?highlight=box%20layout
 class FileMain(BoxLayout):
-    pass
+    def generate(self):
+        dirs = Path.home().iterdir()
+
+        for d in dirs:
+            self.add_widget(File(text=str(d), size_hint=(1, .1)))
 
 
-# Stack Layout Documentation
-# https://kivy.org/doc/stable/api-kivy.uix.stacklayout.html?highlight=layout#kivy.uix.stacklayout.StackLayout.spacing
 class FileBrowser(StackLayout):
     pass
 
 
-# Label Will have to change to own Widget and take in fields such as Date, Time, File Size, etc...
-# https://kivy.org/doc/stable/api-kivy.uix.label.html?highlight=label
-class File(Label):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+class SubBrowser(StackLayout):
+    pass
 
 
-# Box Layout Documentation
-# https://kivy.org/doc/stable/api-kivy.uix.boxlayout.html?highlight=box%20layout
+class File(Button):
+    pass
+
+
 class Footer(BoxLayout):
     pass
 
 
-# App Documentation
-# https://kivy.org/doc/stable/api-kivy.app.html?highlight=app#module-kivy.app
 class AncientTechApp(App):
     def build(self):
         return Main()
