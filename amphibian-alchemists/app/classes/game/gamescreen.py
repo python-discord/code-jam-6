a137e743507b34457d38c729eff84f153bde28cd
YoomamaFTW <andrewcw825@gmail.com>
2020-01-19 02:02:22 -0500

    Sample data added (Comments) * Needs an OnKeyPressedListener or delegate to integrate with the enigma * Enigma logic basically complete but not ready for this PR. This PR is more dedicated towards the data logic

--- amphibian-alchemists/app/classes/game/gamescreen.py
+++ amphibian-alchemists/app/classes/game/gamescreen.py
@@ -1,5 +1,15 @@
 from kivy.uix.screenmanager import Screen
+from kivy.storage.jsonstore import JsonStore
 
 
 class GameScreen(Screen):
-    pass
+    store = JsonStore("data/sample/hello.json")
+    store.put("blah", name="Blah", org="kivy")
+    store.put('tshirtman', name='Gabriel', age=27)
+    store.put('tito', name='Mathieu', age=30)
+    print('tito is', store.get('tito')['age'])
+
+    # or guess the key/entry for a part of the key
+    for item in store.find(name='Gabriel'):
+        print('tshirtmans index key is', item[0])
+        print('his key value pairs are', str(item[1]))
