1be35d539b342b1a3f9e80438cc922ae5d2403f3
Charlie Davies <charlie-jay1@live.co.uk>
2020-01-19 22:15:23 +0000

    Added support managing state
    Used a data controller to allow access to state variables from the game inside kvlang through ctl

--- kaleidoscopic-kings/main.py
+++ kaleidoscopic-kings/main.py
@@ -1,7 +1,11 @@
 from kivy.app import App
-from kivy.properties import StringProperty
+from kivy.event import EventDispatcher
+from kivy.lang import global_idmap
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+
+from cards.main import *
 
 
 class MainWidget(Widget):
@@ -13,8 +17,19 @@ class CardWidget(FloatLayout):
     source = StringProperty()
 
 
+class DataController(EventDispatcher):
+    """manages global state for the app"""
+    active_card = ObjectProperty()
+
+
 class myApp(App):
+    active_card = ObjectProperty()
+
     def build(self):
+        global_idmap["data"] = ctl = DataController()
+        with open("cards/TestCards.json") as f:
+            game = Game([card_from_dict(c) for c in json.load(f)])
+        ctl.active_card = game.deck.get_next_card()
         return MainWidget()
 
 
