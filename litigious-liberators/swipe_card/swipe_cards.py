fb0447ea4437d73e3d88cb24cb25917fb03671c2
aravk33 <aravkasi@gmail.com>
2020-01-19 08:55:27 +0530

    Fix path to pictures directory

--- litigious-liberators/swipe_card/swipe_cards.py
+++ litigious-liberators/swipe_card/swipe_cards.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.carousel import Carousel
+import os
 from os import listdir
 from random import shuffle
 from copy import deepcopy
@@ -12,14 +13,13 @@ class Root(BoxLayout):
 
 
 class Swiper(Carousel):
-
-    def __init__(self,**kwargs):
-        super(Swiper,self).__init__(**kwargs)
-        self.static_pic_list = listdir("../swipe_card_backUP/pictures")
+    def __init__(self, **kwargs):
+        super(Swiper, self).__init__(**kwargs)
+        self.static_pic_list = listdir(os.path.dirname(os.path.abspath(__file__)) + "/pictures")
         self.pic_list = deepcopy(self.static_pic_list)
         shuffle(self.pic_list)
 
-    def shuffle_cards(self,pic_id):
+    def shuffle_cards(self, pic_id):
         self.current_slide.source = f"pictures/{self.pic_list.pop(pic_id)}"
         if len(self.pic_list) == 0:
             self.pic_list = deepcopy(self.static_pic_list)
@@ -30,8 +30,9 @@ class Swiper(Carousel):
         self.next_slide.pic_id = -1
 
     def on_index(self, *args):
-        super(Swiper,self).on_index(self,*args)
+        super(Swiper, self).on_index(self, *args)
         self.shuffle_cards(self.current_slide.pic_id)
+
 
 class CarouselApp(App):
     def build(self):
