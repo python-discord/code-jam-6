8c0cb0a5f0020ca5da960f4b75e4348beb0bf12d
Leterax <noneofyourbuisness@gmail.com>
2020-01-19 21:34:33 +0100

    Improve velocity, jumping and dampening
    velocity is now integrated from acceleration, jumping is only allowed when standing on something and dampening was adjusted for when the player is in the air.

--- brilliant-builders/firestarter/__main__.py
+++ brilliant-builders/firestarter/__main__.py
@@ -11,33 +11,38 @@ class MyGame(Engine):
         super().__init__(**kwargs)
         self.player = Player(self.assets['spritesheet_caveman'], (50, 90))
         self.platform_01 = Platform(self.assets['Untitled'], (50, 20))
-        self.platform_02 = Platform(self.assets['Untitled'], (50 + 32, 20))
-        self.platform_03 = Platform(self.assets['Untitled'], (50 + 32 * 2, 20))
-
-        self.platform_04 = Platform(self.assets['Untitled'], (60 + 32 * 4, 80))
-        self.platform_05 = Platform(self.assets['Untitled'], (60 + 32 * 5, 80))
-        self.platform_06 = Platform(self.assets['Untitled'], (60 + 32 * 6, 80))
+        self.platform_01.change_mode(3)
+        self.platform_02 = Platform(self.assets['Untitled'], (50 + 60, 20))
+        self.platform_02.change_mode(3)
+        self.platform_03 = Platform(self.assets['Untitled'], (50 + 60 * 2, 20))
+        self.platform_03.change_mode(3)
+        self.platform_04 = Platform(self.assets['Untitled'], (50 + 60 * 3, 20))
+        self.platform_04.change_mode(3)
+        self.platform_05 = Platform(self.assets['Untitled'], (50 + 60 * 4, 20))
+        self.platform_05.change_mode(3)
 
         self.coin = PickUpCoin(self.assets['Untitled'], (60 + 32 * 5, 80 + 40))
         self.coin.change_mode(2)
 
-        self.add_sprites([self.platform_01, self.platform_02, self.platform_03,
-                          self.platform_04, self.platform_05, self.platform_06])
-        for s in self.sprites:
-            s.change_mode(3)
-        self.add_sprite(self.player)
-        self.add_sprite(self.coin)
+        self.add_sprites(
+            [self.player,
+             self.coin,
+             self.platform_01, self.platform_02,
+             self.platform_03, self.platform_04,
+             self.platform_05
+             ]
+        )
 
         Clock.schedule_interval(lambda dt: self.player.change_mode(self.player.current_mode + 1), 1)
 
     def update(self, dt: float) -> None:
-        if 'spacebar' in self.pressed_keys:
-            self.player.vel_y = 25
+        if 'spacebar' in self.pressed_keys and self.player.is_standing:
+            self.player.acc_y = 15
 
         if 'a' in self.pressed_keys:
-            self.player.vel_x = -10
+            self.player.vel_x = -7.5
         if 'd' in self.pressed_keys:
-            self.player.vel_x = 10
+            self.player.vel_x = 7.5
 
 
 class Application(App):
