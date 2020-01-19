835635148e2c2a5d512899fd532aaf621f217674
OMotornyi <motorny.sasha@gmail.com>
2020-01-19 03:30:15 +0100

    First commit of rudimentary swiping for pictures.

--- litigious-liberators/swipe_card/swipe_cards.py
+++ litigious-liberators/swipe_card/swipe_cards.py
@@ -1,14 +1,8 @@
 from kivy.app import App
-from kivy.uix.carousel import Carousel
-from kivy.uix.image import AsyncImage
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.carousel import Carousel
 from os import listdir
-from kivy.animation import Animation
-from functools import partial
-from kivy.clock import Clock
 from random import shuffle
 from copy import deepcopy
 
@@ -18,13 +12,10 @@ class Root(BoxLayout):
 
 
 class Swiper(Carousel):
-    card_one = ObjectProperty()
-    card_two = ObjectProperty()
-    card_three = ObjectProperty()
 
     def __init__(self,**kwargs):
         super(Swiper,self).__init__(**kwargs)
-        self.static_pic_list = listdir("pictures")
+        self.static_pic_list = listdir("../swipe_card_backUP/pictures")
         self.pic_list = deepcopy(self.static_pic_list)
         shuffle(self.pic_list)
 
@@ -33,21 +24,14 @@ class Swiper(Carousel):
         if len(self.pic_list) == 0:
             self.pic_list = deepcopy(self.static_pic_list)
         shuffle(self.pic_list)
-        print(self.pic_list)
         self.previous_slide.source = f"pictures/{self.pic_list[0]}"
         self.previous_slide.pic_id = 0
         self.next_slide.source = f"pictures/{self.pic_list[-1]}"
         self.next_slide.pic_id = -1
 
     def on_index(self, *args):
-        print("ON INDEX")
-        print(self._offset)
-        print("BEFORE", self.current_slide.source)
         super(Swiper,self).on_index(self,*args)
-        print("AFTER", self.current_slide.source, self.current_slide.pic_id)
         self.shuffle_cards(self.current_slide.pic_id)
-        print(self.previous_slide.source)
-        print(self.next_slide.source)
 
 class CarouselApp(App):
     def build(self):
