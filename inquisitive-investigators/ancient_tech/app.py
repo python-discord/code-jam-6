b6bb61da4f6debf90aaf17e17ca3e4c406ad4f7f
Monika <devindematto@gmail.com>
2020-01-18 21:00:12 -0500

    Added Quit Popup

--- inquisitive-investigators/ancient_tech/app.py
+++ inquisitive-investigators/ancient_tech/app.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 from datetime import datetime
 from kivy import Config
+from kivy.uix.popup import Popup
 
 Config.set('graphics', 'minimum_width', '1250')
 Config.set('graphics', 'minimum_height', '500')
 Config.set('graphics', 'width', '1250')
 Config.set('graphics', 'height', '500')
 
+from kivy.core.window import Window
 from kivy.app import App
 from kivy.logger import Logger
 from kivy.uix.button import Button
@@ -114,7 +116,52 @@ class NewFile(Button):
 
 
 class Footer(BoxLayout):
-    pass
+    def __init__(self, **kwargs):
+        super(Footer, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == '1':
+            print('1')
+        if keycode[1] == '2':
+            print('2')
+        if keycode[1] == '3':
+            print('3')
+        if keycode[1] == '4':
+            print('4')
+        if keycode[1] == '5':
+            print('5')
+        if keycode[1] == '6':
+            print('6')
+        if keycode[1] == '7':
+            print('7')
+        if keycode[1] == '8':
+            print('8')
+        if keycode[1] == '9':
+            print('9')
+        if keycode[1] == '0':
+            self.quit()
+        return True
+
+    def quit(self):
+        popup = QuitPopup(size_hint=(.5, .5), pos_hint={'center_x': .5, 'center_y': .5})
+        popup.open()
+
+
+class QuitPopup(Popup):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def check(self, value: bool):
+        if not value:
+            self.dismiss()
+        if value:
+            quit(0)
 
 
 class AncientTechApp(App):
