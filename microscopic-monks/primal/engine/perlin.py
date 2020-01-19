81b4716d9c23dd3877869f80a6e1257eae919634
eJi <veryeji@gmail.com>
2020-01-18 20:16:34 -0500

    Implemented pseudo-random map generation done with Perlin noise.

---
+++ microscopic-monks/primal/engine/perlin.py
@@ -0,0 +1,31 @@
+import numpy as np
+from noise import pnoise2
+
+def perlin_array(shape = (30, 30),
+			scale=100, octaves = 6,
+			persistence = 0.5,
+			lacunarity = 2.0,
+			seed = None):
+
+    if not seed:
+
+        seed = np.random.randint(0, 100)
+        print("seed was {}".format(seed))
+
+    arr = np.zeros(shape)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            arr[i][j] = pnoise2(i / scale,
+                                        j / scale,
+                                        octaves=octaves,
+                                        persistence=persistence,
+                                        lacunarity=lacunarity,
+                                        repeatx=1024,
+                                        repeaty=1024,
+                                        base=seed)
+    max_arr = np.max(arr)
+    min_arr = np.min(arr)
+    norm_me = lambda x: (x-min_arr)/(max_arr - min_arr)
+    norm_me = np.vectorize(norm_me)
+    arr = norm_me(arr)
+    return arr
