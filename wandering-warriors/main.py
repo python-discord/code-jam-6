be70b0bcf48fe52cff61f9a6d366ce58763593e9
Cameron Smart <smartcameron@gmail.com>
2020-01-20 01:04:13 -0800

    The box in the bottom right corner can be drawn upon, but there are still bugs to be worked out.

--- wandering-warriors/main.py
+++ wandering-warriors/main.py
@@ -2,13 +2,19 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
-
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Color, Point, GraphicException
+from math import sqrt
 
 class Calculator(Screen):
     pass
 
 
 class Settings(Screen):
+    pass
+
+
+class Screen_Holder(Screen):
     pass
 
 
@@ -27,9 +33,71 @@ class Ledger(Widget):
 class OperationsBar(Widget):
     pass
 
+def calculate_points(x1, y1, x2, y2, steps=1):
+    dx = x2 - x1
+    dy = y2 - y1
+    dist = sqrt(dx * dx + dy * dy)
+    if dist < steps:
+        return
+    o = []
+    m = dist / steps
+    for i in range(1, int(m)):
+        mi = i / m
+        lastx = x1 + dx * mi
+        lasty = y1 + dy * mi
+        o.extend([lastx, lasty])
+    return o
 
-class CuneiformDrawingInput(Widget):
-    pass
+class CuneiformDrawingInput(FloatLayout):
+
+    def on_touch_down(self, touch):
+        ud = touch.ud
+        if(touch.pos[0] > self.pos[0] and 
+           touch.pos[0] < self.pos[0] + self.size[0] and 
+           touch.pos[1] > self.pos[1] and 
+           touch.pos[1] < self.pos[1] + self.size[1]):
+                ud['group'] = g = str(touch.uid)
+                pointsize = 1
+                ud['color'] = 0
+                with self.canvas:
+                    Color(0, 0, 0)
+                    ud['lines'] = [
+                        Point(points=(touch.x, touch.y), source='particle.png',
+                              pointsize=pointsize, group=g)]
+                return True
+        else:
+            ud['lines'] = []
+
+    def on_touch_move(self, touch):
+        try:
+            if(touch.pos[0] > self.pos[0] and 
+               touch.pos[0] < self.pos[0] + self.size[0] and 
+               touch.pos[1] > self.pos[1] and 
+               touch.pos[1] < self.pos[1] + self.size[1]):
+                    ud = touch.ud
+
+                    points = ud['lines'][-1].points
+                    oldx, oldy = points[-2], points[-1]
+                    points = calculate_points(oldx, oldy, touch.x, touch.y)
+                    if points:
+                        try:
+                            lp = ud['lines'][-1].add_point
+                            for idx in range(0, len(points), 2):
+                                lp(points[idx], points[idx + 1])
+                        except GraphicException:
+                            pass
+        except:
+            pass
+        #print(touch.pos)
+
+    def on_touch_up(self, touch):
+        if touch.grab_current is not self:
+            return
+        touch.ungrab(self)
+        ud = touch.ud
+        return (self.canvas.get_group(ud['group'])[1].points)
+        #self.canvas.remove_group(ud['group'])
+        #self.remove_widget(ud['label'])
 
 
 class Screen:
@@ -49,3 +117,10 @@ class CalculatorApp(App):
 
 if __name__ == "__main__":
     CalculatorApp().run()
+''' 
+        drawing_widget = CuneiformDrawingInput()
+        drawing_widget.add_widget(Draw_Pad())
+        holder = Screen_Holder(name="CuneiformDrawingInput")
+        holder.add_widget(drawing_widget)
+        self.sm.add_widget(holder)
+'''
