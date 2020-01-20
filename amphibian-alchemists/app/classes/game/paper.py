8e8608b36801717a46f9a87426a65128b44c3dfc
alex t <sloopooicecream@gmail.com>
2020-01-20 17:05:13 -0500

    added page_screen with RecycleViews, data function doesn't work

--- amphibian-alchemists/app/classes/game/paper.py
+++ amphibian-alchemists/app/classes/game/paper.py
@@ -1,5 +1,8 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.anchorlayout import AnchorLayout
 
 
 class PaperScreen(Screen):
-    pass
+    def input_data(self):
+        print("WORKED!")
+        return [{"text": "DATA!!!!"} for x in range(100)]
