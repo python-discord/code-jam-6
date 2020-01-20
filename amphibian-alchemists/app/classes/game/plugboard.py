3d518446e6023e945791bf053d7331bf7a9a9245
Franccisco <35090042+Franccisco@users.noreply.github.com>
2020-01-19 21:50:20 -0300

    Add a draggable behavior to the plugs and refactoring

--- amphibian-alchemists/app/classes/game/plugboard.py
+++ amphibian-alchemists/app/classes/game/plugboard.py
@@ -1,6 +1,27 @@
+from kivy.factory import Factory
 from kivy.uix.screenmanager import Screen
 
 
 class PlugboardScreen(Screen):
+    plug_count = 0
+
     def plug_handler(self):
         print("CLICK")
+
+    def get_plug(self):
+        if self.plug_count == 0:
+            self.ids.remove_plug.text = "Drag here to remove the plug..."
+        if self.plug_count < 30:
+            plug = Factory.Plug()
+            self.ids.floating_widgets.add_widget(plug)
+
+    def handle_touch_up(self, instance, touch):
+        if instance.collide_point(*touch.pos):
+            if instance.collide_widget(self.ids.remove_plug):
+                self.ids.floating_widgets.remove_widget(instance)
+
+            for floatlayouts in self.ids.plug_board.children:
+                for children in floatlayouts.children:
+                    if instance.collide_widget(children) and type(children) == Factory.PlugHole:
+                        instance.center = children.center
+                        break
