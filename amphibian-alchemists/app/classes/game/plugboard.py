02437ba1da3783fa5354719ac9868a3277485e23
Franccisco <35090042+Franccisco@users.noreply.github.com>
2020-01-20 00:43:41 -0300

    Improve logic and fix errors This commit manages the update of the pair of plugs better and avoid the permanent button blocking when a plug is removed.

--- amphibian-alchemists/app/classes/game/plugboard.py
+++ amphibian-alchemists/app/classes/game/plugboard.py
@@ -4,27 +4,33 @@ from kivy.uix.screenmanager import Screen
 
 
 class PlugboardScreen(Screen):
-    # TODO: detect when plugs_in_screen is incrementing or decrementing
-    # in order to be more precise when plugs are added/releted/moved
     all_plugged = []
-    pairs_of_plugs = []
+    pairs_of_plugs = {}
     plugs_in_screen = BoundedNumericProperty(0, min=0, max=20)
+    last_plugs_count = 0
 
     def plug_handler(self):
         print("CLICK")
 
     def get_plug(self):
         if self.plugs_in_screen == self.property("plugs_in_screen").get_min(self):
-            self.ids.remove_plug.text = "Drag here to remove the plug..."
+            self.ids.remove_plug.text = "Drag the plug here to discard..."
         if self.plugs_in_screen < self.property("plugs_in_screen").get_max(self):
             plug = Factory.Plug()
             self.ids.floating_widgets.add_widget(plug)
+            self.last_plugs_count = self.plugs_in_screen
             self.plugs_in_screen += 1
 
     def handle_touch_up(self, instance, touch):
         if instance.collide_point(*touch.pos):
             if instance.collide_widget(self.ids.remove_plug):
+                for item in self.pairs_of_plugs.items():
+                    if instance.plugged_in in item[1]:
+                        del self.pairs_of_plugs[item[0]]
+                        break
+
                 self.ids.floating_widgets.remove_widget(instance)
+                self.last_plugs_count = self.plugs_in_screen
                 self.plugs_in_screen -= 1
 
             for floatlayouts in self.ids.plug_board.children:
@@ -50,11 +56,21 @@ class PlugboardScreen(Screen):
             if type(plugs) == Factory.Plug and plugs.plugged_in != "":
                 self.all_plugged.append(plugs.plugged_in)
         if len(self.all_plugged) % 2 == 0:
-            self.pairs_of_plugs.append(f"{self.all_plugged[0]}{self.all_plugged[1]}")
+            self.pairs_of_plugs.update(
+                {
+                    f"{self.all_plugged[0]}{self.all_plugged[1]}": [
+                        self.all_plugged[0],
+                        self.all_plugged[1],
+                    ]
+                }
+            )
             self.ids.floating_widgets.get_a_plug.disabled = False
 
     def on_plugs_in_screen(self, instance, value):
         if value % 2 == 0:
-            self.ids.floating_widgets.get_a_plug.disabled = True
+            if self.last_plugs_count > self.plugs_in_screen:
+                self.ids.floating_widgets.get_a_plug.disabled = False
+            else:
+                self.ids.floating_widgets.get_a_plug.disabled = True
         else:
             self.ids.floating_widgets.get_a_plug.disabled = False
