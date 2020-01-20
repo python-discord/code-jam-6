0acdff57f8b4d1d2c1f7a885ec9a1af6ce424516
Franccisco <35090042+Franccisco@users.noreply.github.com>
2020-01-20 20:28:24 -0300

    Try a new fix to avoid repetitive calls to input_data()

--- amphibian-alchemists/app/classes/game/paper.py
+++ amphibian-alchemists/app/classes/game/paper.py
@@ -1,7 +1,16 @@
 from kivy.uix.screenmanager import Screen
+from kivy.clock import Clock
 
 
 class PaperScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Clock.schedule_once(
+            lambda dt: setattr(
+                self.ids.input_paper.recycle_view, "data", self.input_data()
+            )
+        )
+
     def input_data(self):
         print("WORKED!")
         return [{"text": "DATA!!!!"} for x in range(100)]
