19d0516dad70f996667b0ea5e468e539bd93326d
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2020-01-20 03:35:16 +0100

    Initialize the game instance

--- excellent-exorcists/src/main.py
+++ excellent-exorcists/src/main.py
@@ -1,10 +1,16 @@
-from kivy.app import App
+from src.core.game import Game
+from src.core.game_loop import GameLoop
+from src.core.screen_manager import ScreenManager
 
+from src.screens.game_screen import GameScreen
 
-class GameApp(App):
-	
-	def build(self):
-		root = self.root
-	
 if __name__ == '__main__':
-	GameApp().run()
+    loop = GameLoop()
+    screen_manager = ScreenManager()
+    screen_manager.add_screen(GameScreen)
+    screen_manager.set_current_screen(GameScreen)
+
+    loop.start(60)
+    game = Game(loop, screen_manager)
+
+    game.run()
