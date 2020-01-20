fea202cd4420147547ae423b0596021c2a39fdc8
YoomamaFTW <andrewcw825@gmail.com>
2020-01-19 22:43:36 -0500

    Buttons can select. Recycle can't see

--- amphibian-alchemists/app/classes/gameselector.py
+++ amphibian-alchemists/app/classes/gameselector.py
@@ -3,16 +3,69 @@ import os
 from kivy.app import App
 from kivy.storage.jsonstore import JsonStore
 from kivy.uix.screenmanager import Screen
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.label import Label
+from kivy.properties import BooleanProperty
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 
 DATA_DIR = os.path.join(
     App.get_running_app().APP_DIR, os.path.normcase("data/gamestate.json")
 )
 
 
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
+                                 RecycleBoxLayout):
+    ''' Adds selection and focus behaviour to the view. '''
+
+
+class SelectableLabel(RecycleDataViewBehavior, Label):
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableLabel, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableLabel, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+        if is_selected:
+            print("selection changed to {0}".format(rv.data[index]))
+        else:
+            print("selection removed for {0}".format(rv.data[index]))
+
+
 class GameSelectorScreen(Screen):
     def on_enter(self, *args):
-        print("OK")
+        if not os.path.exists(DATA_DIR):
+            store = JsonStore(DATA_DIR)
+            store.put("latest_game_id", id=None)
 
     def populate(self):
+        from string import ascii_lowercase
+        from random import sample
+        self.rv.data = [{'value': ''.join(sample(ascii_lowercase, 6))}
+                        for x in range(50)]
         store = JsonStore(DATA_DIR)
+        store.get("latest_game_id")["id"]
+
+    def start_new_game(self):
+        print(1)
+
+    def load_game(self):
+        print(1)
 
