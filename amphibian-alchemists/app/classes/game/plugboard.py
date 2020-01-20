00b8f5bc2cb24682540e3f9405ad5b332a1399af
Franccisco <35090042+Franccisco@users.noreply.github.com>
2020-01-20 15:37:15 -0300

    Add initial wire support

--- amphibian-alchemists/app/classes/game/plugboard.py
+++ amphibian-alchemists/app/classes/game/plugboard.py
@@ -1,15 +1,15 @@
 from kivy.animation import Animation
 from kivy.factory import Factory
 from kivy.metrics import dp
-from kivy.properties import BoundedNumericProperty
+from kivy.properties import BoundedNumericProperty, DictProperty
 from kivy.uix.screenmanager import Screen
 
 
 class PlugboardScreen(Screen):
     all_plugged = []
-    pairs_of_plugs = {}
     plugs_in_screen = BoundedNumericProperty(0, min=0, max=20)
     last_plugs_count = 0
+    wires = DictProperty({})
 
     def get_plug(self):
         if self.plugs_in_screen == self.property("plugs_in_screen").get_min(self):
@@ -52,20 +52,19 @@ class PlugboardScreen(Screen):
             if isinstance(plug, Factory.Plug) and plug.plugged_in != "":
                 self.all_plugged.append(plug.plugged_in)
         if len(self.all_plugged) >= 2 and len(self.all_plugged) % 2 == 0:
-            self.pairs_of_plugs.update(
-                {
-                    f"{self.all_plugged[0]}{self.all_plugged[1]}": [
-                        self.all_plugged[0],
-                        self.all_plugged[1],
-                    ]
-                }
-            )
-
+            wire = Factory.Wire()
+            wire.points = [
+                *self.ids.plug_board.ids[self.all_plugged[0]].center,
+                *self.ids.plug_board.ids[self.all_plugged[1]].center,
+            ]
+            self.ids.floating_widgets.add_widget(wire)
+            self.wires.update({f"{''.join(self.all_plugged[:2])}": wire})
             self.ids.floating_widgets.get_a_plug.disabled = False
 
     def on_plugged_out(self, instance):
         if instance.plugged_in in self.all_plugged:
             self.all_plugged.remove(instance.plugged_in)
+            self.delete_from_pair(instance)
         instance.plugged_in == ""
         self.animate_positioning(instance)
         return True
@@ -75,9 +74,10 @@ class PlugboardScreen(Screen):
         anim.start(instance)
 
     def delete_from_pair(self, instance):
-        for item in self.pairs_of_plugs.items():
-            if instance.plugged_in in item[1]:
-                del self.pairs_of_plugs[item[0]]
+        for item in self.wires.items():
+            if instance.plugged_in in item[0]:
+                self.ids.floating_widgets.remove_widget(self.wires[item[0]])
+                del self.wires[item[0]]
                 break
 
     def on_plugs_in_screen(self, instance, value):
@@ -106,3 +106,20 @@ class PlugboardScreen(Screen):
                         self.ids.plug_board.plug_reference.size[0] - dp(10),
                         self.ids.plug_board.plug_reference.size[1] - dp(10),
                     )
+            for wire in self.wires.items():
+                wire[1].points = [
+                    *self.ids.plug_board.ids[wire[0][0]].center,
+                    *self.ids.plug_board.ids[wire[0][1]].center,
+                ]
+
+    def handle_wire_pos(self, instance, touch):
+        if self.collide_point(*touch.pos) and self.wires and instance.plugged_in:
+            for item in self.wires.items():
+                if instance.plugged_in in item[0]:
+                    moving_plug = 0
+                    if item[0].index(instance.plugged_in) != 0:
+                        moving_plug = 2
+
+                    item[1].points[moving_plug] = instance.center[0]
+                    item[1].points[moving_plug + 1] = instance.center[1]
+                    break
