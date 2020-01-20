0fa8427390c575726d817a323ed182720b65568e
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2020-01-20 20:47:04 +0100

    Add pressed keys listeners

--- excellent-exorcists/src/core/screen.py
+++ excellent-exorcists/src/core/screen.py
@@ -1,11 +1,23 @@
 from kivy.graphics.instructions import RenderContext
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
 
 
 class Screen(Widget):
     def __init__(self, **kwargs):
         self.canvas = RenderContext()
+        self.pressed_keys = set()
+
+        Window.bind(on_key_down=self.key_down)
+        Window.bind(on_key_up=self.key_up)
+
         super().__init__(**kwargs)
+
+    def key_down(self, _, key, *args):
+        self.pressed_keys.add(key)
+
+    def key_up(self, _, key, *args):
+        self.pressed_keys.discard(key)
 
     def render(self, delta):
         """Override this method and render graphics"""
