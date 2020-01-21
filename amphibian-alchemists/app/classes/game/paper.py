276959acd4a5fcfff6bd37844beaa96f9fddbc2f
alex t <sloopooicecream@gmail.com>
2020-01-20 19:43:24 -0500

    input paper get data when game is opened, output paper gets data everytime page is loaded

--- amphibian-alchemists/app/classes/game/paper.py
+++ amphibian-alchemists/app/classes/game/paper.py
@@ -6,11 +6,19 @@ class PaperScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         Clock.schedule_once(
-            lambda dt: setattr(
-                self.ids.input_paper.recycle_view, "data", self.input_data()
-            )
+            lambda dt: setattr(self.ids.input_paper.recycle_view, "data", self.input_data())
         )
 
+    # This only runs when the game is first opened (settings sheet)
     def input_data(self):
-        print("WORKED!")
-        return [{"text": "DATA!!!!"} for x in range(100)]
+        input_data = [
+            {"text": "Plugboard: "},
+            {"text": "Rotors: "},
+            {"text": "Rotor settings: "},
+            {"text": "Ciphertext: "},
+        ]
+        return input_data
+
+    # This paper's data is changed everytime the user comes back
+    def output_data(self):
+        return [{"text": "What the user has typed: "}]
