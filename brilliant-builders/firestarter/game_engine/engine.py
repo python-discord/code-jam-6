39c4f54bbcd01e2e5439264290874dd07883d1a4
Akarys42 <Akarys42@users.noreply.github.com>
2020-01-21 19:03:55 +0100

    Move sprite loading to the resource loader

--- brilliant-builders/firestarter/game_engine/engine.py
+++ brilliant-builders/firestarter/game_engine/engine.py
@@ -1,16 +1,12 @@
-import os
 from pathlib import Path
 from typing import List
 
-from firestarter.game_engine.sprite import Sprite, SpriteConfig
+from firestarter.game_engine.resources_loader import load_resources
+from firestarter.game_engine.sprite import Sprite
 
 from kivy.clock import Clock
-from kivy.core.image import Image as CoreImage
 from kivy.core.window import Keyboard, Window
-from kivy.logger import Logger
 from kivy.uix.widget import Widget
-
-import toml
 
 IMAGE_EXTENSIONS = ['png']
 
@@ -32,50 +28,11 @@ class Engine(Widget):
         self.sprites: List[Sprite] = []  # TODO: make this only accept sprite classes
 
         # dict of assets
-        self.assets = self.load_sprite_sheets()
+        self.assets = load_resources()[0]
 
         # call the update method every frame
         Clock.schedule_interval(self._update, 1.0 / 60.0)
         Clock.schedule_interval(self._animate, 1.0 / 10.0)
-
-    def load_sprite_sheets(self) -> dict:
-        """Load all images in the resources/sprites directory to the gpu.
-
-        Every image should have an associated config file named <file>_config.toml
-        containing a section `animation` with the following data :
-        * size : Two elements array, size of each sprite
-        * modes : Number of animations in the spritesheet
-        * frame_count : Array, number of frames per animation"""
-        assets = {}
-        for sprite_sheet in [
-            sp for sp in os.listdir(self.resource_dir / 'sprites')
-            if sp.rsplit('.', 1)[-1] in IMAGE_EXTENSIONS
-        ]:
-            img_path = (self.resource_dir / 'sprites' / sprite_sheet).as_posix()
-            config_file = (
-                self.resource_dir / 'sprites' / (sprite_sheet.rsplit('.', 1)[0] + '_config.toml')
-            ).as_posix()
-            if not os.path.exists(config_file):
-                Logger.error(
-                    f"Engine: No configuration file found for {sprite_sheet}, not loading it."
-                )
-                continue
-
-            texture = CoreImage(img_path).texture
-            texture.mag_filter = 'nearest'
-
-            with open(config_file) as f:
-                config_dict = toml.load(f)['animation']
-
-            config = SpriteConfig(
-                self.resource_dir / 'sprites' / sprite_sheet,
-                texture,
-                config_dict['size'],
-                config_dict['modes'],
-                config_dict['frame_count']
-            )
-            assets[sprite_sheet.rsplit('.', 1)[0]] = config
-        return assets
 
     def add_sprite(self, sprite: Sprite) -> None:
         """Add the sprite to the internal list and add the widget."""
---
+++ brilliant-builders/firestarter/game_engine/resources_loader.py
@@ -0,0 +1,62 @@
+import os
+from pathlib import Path
+from typing import Any, Dict, List, Optional
+
+from firestarter.game_engine.sprite import SpriteConfig
+
+from kivy.core.image import Image as CoreImage
+from kivy.logger import Logger
+
+import toml
+
+RESOURCE_DIR = (Path('.') / 'firestarter' / 'resources').absolute()
+RESOURCE_TYPES = ['sprites']
+
+
+def load_sprite(folder_path: Path, sp: str) -> Optional[SpriteConfig]:
+    if sp.rsplit('.', 1)[-1] != 'png':
+        return  # It is probably a config file, not loading it
+
+    config_file = (folder_path / sp.rsplit('.', 1)[0]).as_posix() + '_config.toml'
+    sp_path = folder_path / sp
+
+    if not os.path.exists(config_file):
+        Logger.error(f"Engine: No configuration file found for {sp}, not loading it.")
+        return
+
+    texture = CoreImage(sp_path.as_posix()).texture
+    texture.mag_filter = 'nearest'
+
+    with open(config_file) as f:
+        config_dict = toml.load(f)['animation']
+
+    config = SpriteConfig(
+        sp_path,
+        texture,
+        config_dict['size'],
+        config_dict['modes'],
+        config_dict['frame_count']
+    )
+    return config
+
+
+def load_resources() -> List[Dict[str, Any]]:
+    """Generate a list of loaded resources."""
+    loaded_resources_list = []
+
+    for rtype in RESOURCE_TYPES:
+        loaded_resources = {}
+        resource_dir_path = RESOURCE_DIR / rtype
+
+        for resource in os.listdir(resource_dir_path):
+            loaded = LOADER_MAPPING[rtype](resource_dir_path, resource)
+            if loaded:
+                loaded_resources[resource.rsplit('.', 1)[0]] = loaded
+
+        loaded_resources_list.append(loaded_resources)
+    return loaded_resources_list
+
+
+LOADER_MAPPING = {
+    'sprites': load_sprite
+}
