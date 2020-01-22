ea0b92c22338dc32722a1cbdeedba42f41cb166b
CharlieADavies <charlieadavies@gmail.com>
2020-01-22 02:49:16 +0000

    added callbacks to make choice selection \n currently using dud callback functions, but the functionality is there to easily link them with real functions

--- kaleidoscopic-kings/main.py
+++ kaleidoscopic-kings/main.py
@@ -5,7 +5,7 @@ from kivy.event import EventDispatcher
 from kivy.lang import global_idmap
 from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
+from kivy.config import Config
 
 from cards.main import Game, card_from_dict
 
@@ -14,7 +14,7 @@ class MainWidget(BoxLayout):
     pass
 
 
-class CardWidget(FloatLayout):
+class CardBoxLayout(BoxLayout):
     text = StringProperty()
     source = StringProperty()
 
@@ -22,7 +22,11 @@ class CardWidget(FloatLayout):
 class DataController(EventDispatcher):
     """manages global state for the app"""
 
-    active_card = ObjectProperty()
+    pass
+
+
+Config.set("graphics", "width", "600")
+Config.set("graphics", "height", "800")
 
 
 class myApp(App):
@@ -30,9 +34,20 @@ class myApp(App):
 
     def build(self):
         global_idmap["data"] = ctl = DataController()
+
         with open("cards/TestCards.json") as f:
             game = Game([card_from_dict(c) for c in json.load(f)])
-        ctl.active_card = game.deck.get_next_card()
+        card = game.deck.get_next_card()
+
+        # TODO replace with real state
+        ctl.placeholder_art = "./assets/bonfire.png"
+        ctl.active_card = card
+
+        # TODO set these with a function reference to the game somewhere
+        # TODO we can also use the same callbacks for swiping.
+        ctl.choice_1_callback = lambda: print("clicked 1")
+        ctl.choice_2_callback = lambda: print("clicked 2")
+
         return MainWidget()
 
 
