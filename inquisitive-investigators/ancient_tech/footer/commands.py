9fce3424d2d1be9c9b28bd5566e68ca313880216
Monika <devindematto@gmail.com>
2020-01-21 20:55:35 -0500

    "Added MKDIR"

--- inquisitive-investigators/ancient_tech/footer/commands.py
+++ inquisitive-investigators/ancient_tech/footer/commands.py
@@ -1,13 +1,15 @@
 from typing import Any
+
+from kivy.properties import NumericProperty
 from kivy.uix.popup import Popup
-from kivy.properties import ObjectProperty
+import pathlib
 
 
 class BasePopup(Popup):
-    
+
     def __init__(
             self, ctx: 'Footer', *args: Any, **kwargs: Any
-        ) -> None:
+    ) -> None:
         super().__init__(*args, **kwargs)
         self.ctx = ctx
 
@@ -33,9 +35,33 @@ class AboutPopup(BasePopup):
 
 
 class Mkdir(BasePopup):
+    left = NumericProperty()
+    right = NumericProperty()
 
-    def mkdir(self) -> None:
-        print(self.ids.create.text)
+    def __init__(self, ctx: 'Footer', *args: Any, **kwargs: Any):
+        super().__init__(ctx, *args, **kwargs)
+        self.filemanager = 0
+
+    def buttonselect(self, manager):
+        self.left, self.right = 0, 0
+        self.filemanager = manager
+        if manager == 1:
+            self.left = .5
+        else:
+            self.right = .5
+
+    def mkdir(self, dir_name: str) -> None:
+        if self.filemanager != 0 or dir_name != '':
+            if self.filemanager == 1:
+                new_dir = pathlib.Path(self.ctx.parent.ids.left.ids.header.ids.directory.text)
+            else:
+                new_dir = pathlib.Path(self.ctx.parent.ids.right.ids.header.ids.directory.text)
+            new_dir /= dir_name
+            new_dir.mkdir()
+            self.dismiss()
+        else:
+            print('Please Enter a File Manager or Directory Name')
+
 
 
 class QuitPopup(BasePopup):
