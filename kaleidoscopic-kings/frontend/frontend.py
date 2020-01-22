665b2a6a968f8bf87e2987a15d4f5e753502cc40
CharlieADavies <charlieadavies@gmail.com>
2020-01-22 22:00:04 +0000

    Added basic linkage of state to allow the user to press a button and choose an option

--- kaleidoscopic-kings/frontend/frontend.py
+++ kaleidoscopic-kings/frontend/frontend.py
@@ -8,7 +8,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.config import Config
 from kivy.uix.gridlayout import GridLayout
 
-from cards.main import Game, card_from_dict
+from backend.card_format import Card
+from backend.main import Game
 
 
 class MainWidget(GridLayout):
@@ -20,7 +21,9 @@ class StatusBar(BoxLayout):
 
 
 class CardWidget(BoxLayout):
-    """Widget that contains a card, its art, and buttons to make the relevant choices"""
+    """
+    Widget that contains a card, art, and buttons to make the relevant choices
+    """
 
     pass
 
@@ -34,6 +37,35 @@ class CardBoxLayout(BoxLayout):
 class DataController(EventDispatcher):
     """manages global state for the app"""
 
+    active_card: Card = ObjectProperty(rebind=True)
+    active_text = StringProperty()
+    game: Game = ObjectProperty()
+
+    def choice_handler(self, choice):
+        """Used to update state for the app when the user makes a choice"""
+        if choice == "1" or len(self.active_card.options) == 1:
+            outcome = self.active_card.options[0].get_outcome()
+            self.active_card = self.game.take_turn(outcome)
+            self.active_text = self.active_card.card_id
+            print(f"User is making choice {choice} on card {self.active_card}")
+        else:
+            outcome = self.active_card.options[0].get_outcome()
+            self.active_card = self.game.take_turn(outcome)
+            self.active_text = self.active_card.card_id
+            print(f"User is making choice {choice} on card {self.active_card}")
+
+    def get_active_card(self):
+        return self.active_card
+
+
+def load_game() -> Game:
+    """Loads the backend and returns a game object"""
+    with open("../backend/data/example_game_cards.json") as f:
+        _cards = [Card(**card_dict) for card_dict in json.load(f)]
+    with open("../backend/data/example_game_states.json") as f:
+        _game_states = json.load(f)
+    return Game(_cards, _game_states)
+
 
 class CardGameApp(App):
     active_card = ObjectProperty()
@@ -43,19 +75,9 @@ class CardGameApp(App):
         Config.set("graphics", "height", "900")
 
         global_idmap["data"] = ctl = DataController()
-        with open("../cards/TestCards.json") as f:
-            game = Game([card_from_dict(c) for c in json.load(f)])
-        card = game.deck.get_next_card()
 
-        # TODO replace with real state
-        ctl.placeholder_art = "../assets/bonfire_icon.png"
-        ctl.active_card = card
-
-        # TODO set these with a function reference to the game somewhere
-        # TODO we can also use the same callbacks for swiping.
-        ctl.choice_1_callback = lambda: print("clicked 1")
-        ctl.choice_2_callback = lambda: print("clicked 2")
-
+        ctl.game = game = load_game()
+        ctl.active_card = game.start_game()
         return MainWidget()
 
 
