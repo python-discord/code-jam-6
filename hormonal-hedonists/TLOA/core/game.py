af44b8b0a4abb9b4d1066fe84bc074f0fdb8d651
Pippi <tienlocnguyen.ac@gmail.com>
2020-01-23 20:51:08 +0700

    Init code for ship

--- hormonal-hedonists/TLOA/core/game.py
+++ hormonal-hedonists/TLOA/core/game.py
@@ -1,14 +1,45 @@
 from TLOA.core.constants import Actions
 from TLOA.entities import MirrorCannon
+from TLOA.entities import GoldenShip, BrownShip
+
+from kivy import Logger
+from kivy.clock import Clock
 
 
 class Game:
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.mirror = MirrorCannon()
+        self.ships = []
+        self.total_spawned_ship = 0
+        self.view = None
 
-    def start(self):
-        pass
+    def start(self, view):
+        Logger.info('Start game')
+        self.view = view
+        Clock.schedule_interval(self.spawn_ship, 5)
+
+    def spawn_ship(self, dt):
+        self.total_spawned_ship += 1
+        # TODO add code for ship position
+        # TODO instead of create new ship, we can use existing one, so no need allocate new memory
+        if self.total_spawned_ship % 10 == 0:
+            # after 9 brown ships, we add new 1 golden ship
+            Logger.info('Spawn new golden ship')
+            new_ship = GoldenShip()
+        else:
+            Logger.info('Spawn new brown ship')
+            new_ship = BrownShip()
+        new_ship.bind(destroyed=self.on_ship_destroyed)
+        self.ships.append(new_ship)
+        self.view.show_ship(new_ship)
+
+    def on_ship_destroyed(self, instance, new_state):
+        if new_state:
+            # ship is destroyed, remove it from view
+            Logger.info('Remove ship')
+            self.ships.remove(instance)
+            # TODO update score
 
     def process_action(self, action: Actions):
         if action == Actions.MOVE_LEFT:
--- hormonal-hedonists/TLOA/entities/ship.py
+++ hormonal-hedonists/TLOA/entities/ship.py
@@ -1,5 +1,47 @@
 from TLOA.entities import Entity
+from kivy.properties import BoundedNumericProperty, StringProperty, BooleanProperty
+
+from enum import Enum
+
+MIN_RES = 1
+MAX_RES = 10
+
+MAX_HEALTH = 100
+
+MIN_SPEED = 5
+MAX_SPEED = 10
+
+
+class ShipType(Enum):
+    BROWN = 0
+    GOLDEN = 1
 
 
 class Ship(Entity):
-    pass
+    _type = None
+    health = BoundedNumericProperty(MAX_HEALTH, min=0, max=MAX_HEALTH,
+                                    errorhandler=lambda x: 0 if x < 0 else MAX_HEALTH)
+    fire_resistance = BoundedNumericProperty(MIN_RES, min=MIN_RES, max=MAX_RES,
+                                             errorhandler=lambda x: MIN_RES if x < MIN_RES else MAX_RES)
+    speed = BoundedNumericProperty(MIN_SPEED, min=MIN_SPEED, max=MAX_SPEED,
+                                    errorhandler=lambda x: MIN_RES if x < MIN_RES else MAX_RES)
+    destroyed = BooleanProperty(False)
+
+    def take_damage(self, damage: int):
+        # calculate health base on fire resistance. Can improve later
+        health -= int(damage / self.fire_resistance)
+    
+    def on_health(self, instance, value):
+        if self.health == 0:
+            self.destroyed = True
+
+class GoldenShip(Ship):
+    _type = ShipType.GOLDEN
+    fire_resistance = MAX_RES
+    speed = MIN_SPEED
+
+
+class BrownShip(Ship):
+    _type = ShipType.BROWN
+    fire_resistance = MIN_RES
+    speed = MAX_SPEED
