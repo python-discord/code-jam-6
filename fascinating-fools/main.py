b4d31382410a31062f5f6d7b3f703d3744a87f3a
Gideon Maina <maina.gidi@gmail.com>
2020-01-23 14:37:44 +0300

    task: - Add transition from start -> home -> trivia widget -> results widget samples

--- fascinating-fools/main.py
+++ fascinating-fools/main.py
@@ -1,5 +1,5 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
@@ -40,13 +40,6 @@ class ButtonWithSound(Button):
             "Medicine": MedicineTriviaLayout()
         }
 
-    def on_press(self):
-        self.button_press_sound.play()
-        # switch window to the category clicked
-        print(f"Selected category {self.category}")
-        category_widget = self.category_map.get(self.category)
-        self.parent.add_widget(category_widget)
-
 
 class Background(Widget):
     def __init__(self, source, **kwargs):
@@ -86,7 +79,13 @@ class SecondGridRow(BoxLayout):
             size_hint=(.3, 1),
             sound_file='sounds/button-press-whoosh.wav',
         )
+        self.category_one.bind(on_press=self.handle_category_selection)
         self.add_widget(self.category_one)
+
+    def handle_category_selection(self, pressed):
+        print(f"{pressed}")
+        pressed.button_press_sound.play()
+        self.parent.parent.manager.current = 'home_screen'
 
 
 class ThirdGridRow(BoxLayout):
@@ -113,32 +112,124 @@ class FourthGridRow(BoxLayout):
         self.add_widget(self.category_seven)
 
 
-class StartTriviaHomeLayout(GridLayout):
+class StartTriviaHomeLayout(Screen):
     def __init__(self, **kwargs):
         super(StartTriviaHomeLayout, self).__init__(**kwargs)
-        self.rows = 4
-        self.padding = (200, 0, 200, 100)
-        self.spacing = [20, 10]
+        self.layout = GridLayout()
+        self.layout.rows = 4
+        self.layout.padding = (200, 0, 200, 100)
+        self.layout.spacing = [20, 10]
         # add the background TODO: for some reason distorts the grid
         # self.background = Background(source='background.jpg')
-        # self.add_widget(self.background)
+        # self.layout.add_widget(self.background)
         # first row
         self.first_grid = FirstGridRow()
-        self.add_widget(self.first_grid)
+        self.layout.add_widget(self.first_grid)
         # second row
         self.second_grid = SecondGridRow()
-        self.add_widget(self.second_grid)
+        self.layout.add_widget(self.second_grid)
         # third row
         self.third_grid = ThirdGridRow()
-        self.add_widget(self.third_grid)
+        self.layout.add_widget(self.third_grid)
         # fourth row
         self.fourth_grid = FourthGridRow()
-        self.add_widget(self.fourth_grid)
+        self.layout.add_widget(self.fourth_grid)
+
+        # Add the above layout to this screen
+        self.add_widget(self.layout)
+
+
+class MenuScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MenuScreen, self).__init__(**kwargs)
+        self.layout = GridLayout()
+        self.layout.rows = 2
+        self.layout.padding = (200, 0, 200, 100)
+        self.layout.spacing = [20, 10]
+        self.layout.add_widget(Label(text='Ready to go ? Hit start below.'))
+        self.start_button = Button(text='Start')
+        self.start_button.bind(on_press=self.press_start_button)
+        self.layout.add_widget(self.start_button)
+        # Add the above layout to this screen
+        self.add_widget(self.layout)
+
+    def press_start_button(self, touch):
+        print(f"{touch.__dict__}")
+        self.manager.current = 'play_screen'
+
+
+class PlayScreen(Screen):
+    def __init__(self, **kwargs):
+        super(PlayScreen, self).__init__(**kwargs)
+        self.layout = GridLayout()
+        self.layout.rows = 5
+        self.layout.padding = (200, 0, 200, 100)
+        self.layout.spacing = [20, 10]
+        self.layout.add_widget(
+            Label(text='Which of these is an East African country?'))
+        # Create answer selections
+        self.first_answer = Button(text='A: Kenya')
+        self.second_answer = Button(text='B: Hawaii')
+        self.third_answer = Button(text='C: USA')
+        self.fourth_answer = Button(text='D: Mauritius')
+        # Bind the actions
+        self.first_answer.bind(on_press=self.check_answer)
+        self.second_answer.bind(on_press=self.check_answer)
+        self.third_answer.bind(on_press=self.check_answer)
+        self.fourth_answer.bind(on_press=self.check_answer)
+
+        # Add to the screen
+        self.layout.add_widget(self.first_answer)
+        self.layout.add_widget(self.second_answer)
+        self.layout.add_widget(self.third_answer)
+        self.layout.add_widget(self.fourth_answer)
+        # Add the above layout to this screen
+        self.add_widget(self.layout)
+
+    def check_answer(self, press):
+        print(f"{press}- Selected answer {press.text}")
+        # If this is the last question then switch to the last
+        self.manager.current = 'results_screen'
+
+
+class ResultsScreen(Screen):
+    def __init__(self, **kwargs):
+        super(ResultsScreen, self).__init__(**kwargs)
+        self.layout = GridLayout()
+        self.layout.rows = 5
+        self.cols = 1
+        self.layout.padding = (200, 0, 200, 100)
+        self.layout.spacing = [20, 10]
+        self.layout.add_widget(
+            Label(text='Quiz complete', font_size=50, size_hint=(1, 0)))
+        self.layout.add_widget(Label(text='Score: 340', font_size=40))
+        self.layout.add_widget(
+            Label(text='Personal Best 345 points! Congrats !!', font_size=35))
+        self.layout.add_widget(
+            Label(text='Correct on first attempt is 9/10', font_size=25))
+        self.layout.add_widget(
+            Label(text='Your rank on the Leaderboard is 1 out of 100',
+                  font_size=25))
+        self.restart = Button(text='Restart')
+        self.restart.bind(on_press=self.handle_restart)
+        self.layout.add_widget(self.restart)
+        # Add the above layout to this screen
+        self.add_widget(self.layout)
+
+    def handle_restart(self, press):
+        print(f"Press {press}")
+        # Go back to the initial screen
+        self.manager.current = 'home_screen'
 
 
 class AncientTriviaApp(App):
     def build(self):
-        return StartTriviaHomeLayout()
+        screen_manager = ScreenManager(transition=FadeTransition())
+        screen_manager.add_widget(StartTriviaHomeLayout(name='start_page'))
+        screen_manager.add_widget(MenuScreen(name='home_screen'))
+        screen_manager.add_widget(PlayScreen(name='play_screen'))
+        screen_manager.add_widget(ResultsScreen(name='results_screen'))
+        return screen_manager
 
 
 if __name__ == '__main__':
