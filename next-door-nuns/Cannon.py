794aeebef61f10976e4302db4e9b3db64cf86076
Alexander <amaroudis@gmail.com>
2020-01-23 18:26:22 -0500

    Added functionality to change angle of cannon

--- next-door-nuns/Cannon.py
+++ next-door-nuns/Cannon.py
@@ -3,6 +3,9 @@ from kivy.properties import Clock
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle
+from kivy.graphics import Rotate
+from kivy.uix.image import Image
+from math import sin,cos,radians
 
 
 class Cannon(Widget):
@@ -19,11 +22,17 @@ class Cannon(Widget):
                 self.velocity = int(velocity)
                 print(self.velocity)
 
+    def set_angle(self,angle):
+        print(angle)
+        angle_int_deg = int(angle)
+        self.angle_y = sin(radians(angle_int_deg))
+        self.angle_x = cos(radians(angle_int_deg))
+
     def fire_cannon(self):
         self.cannonball.pos = (50, 50)
         self.velocity_clock = Clock.schedule_interval(self.drop, 0.1)
-        self.velocity_y = self.velocity
-        self.velocity_x = self.velocity
+        self.velocity_y = self.velocity * self.angle_y
+        self.velocity_x = self.velocity * self.angle_x
     def drop(self, dt):
 
         if self.velocity_x >= 0:
