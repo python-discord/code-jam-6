bf60e309690c4b1fb7ff4335f1c5cd278b99ebff
CharlieADavies <charlieadavies@gmail.com>
2020-01-23 21:06:47 +0000

    Moved state management out of frontend.py and into main.py

--- kaleidoscopic-kings/frontend/frontend.py
+++ kaleidoscopic-kings/frontend/frontend.py
@@ -1,8 +1,3 @@
-from backend.card_format import Card
-from backend.main import Game
-
-from kivy.properties import ObjectProperty
-from kivy.event import EventDispatcher
 from kivy.properties import StringProperty
 
 from kivy.uix.boxlayout import BoxLayout
@@ -29,26 +24,3 @@ class CardBoxLayout(BoxLayout):
     """Standard box layout with a stone tablet as a background"""
 
     source = StringProperty()
-
-
-class DataController(EventDispatcher):
-    """Manages global state for the app"""
-
-    active_card: Card = ObjectProperty(rebind=True)
-    active_text = StringProperty()
-    game: Game = ObjectProperty()
-    assets_loc = StringProperty("assets/")
-
-    def choice_handler(self, choice):
-        """Used to update state for the app when the user makes a choice"""
-        if choice == "1" or len(self.active_card.options) == 1:
-            outcome = self.active_card.options[0].get_outcome()
-            self.active_card = self.game.take_turn(outcome)
-            self.active_text = self.active_card.card_id
-        else:
-            outcome = self.active_card.options[0].get_outcome()
-            self.active_card = self.game.take_turn(outcome)
-            self.active_text = self.active_card.card_id
-
-    def get_active_card(self):
-        return self.active_card
--- kaleidoscopic-kings/main.py
+++ kaleidoscopic-kings/main.py
@@ -1,10 +1,36 @@
-from frontend.frontend import DataController, MainWidget
-from backend.main import load_game
+from kivy.event import EventDispatcher
+
+from backend.card_format import Card
+from frontend.frontend import MainWidget
+from backend.main import load_game, Game
 
 from kivy.app import App
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.config import Config
 from kivy.lang import global_idmap
+
+
+class DataController(EventDispatcher):
+    """Manages global state for the app"""
+
+    active_card: Card = ObjectProperty(rebind=True)
+    active_text = StringProperty()
+    game: Game = ObjectProperty()
+    assets_loc = StringProperty("assets/")
+
+    def choice_handler(self, choice):
+        """Used to update state for the app when the user makes a choice"""
+        if choice == "1" or len(self.active_card.options) == 1:
+            outcome = self.active_card.options[0].get_outcome()
+            self.active_card = self.game.take_turn(outcome)
+            self.active_text = self.active_card.card_id
+        else:
+            outcome = self.active_card.options[0].get_outcome()
+            self.active_card = self.game.take_turn(outcome)
+            self.active_text = self.active_card.card_id
+
+    def get_active_card(self):
+        return self.active_card
 
 
 class CardGameApp(App):
