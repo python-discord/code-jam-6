48d74062fece201dff5f81e5b2b4d0102bd170ea
Franccisco <35090042+Franccisco@users.noreply.github.com>
2020-01-24 10:46:51 -0300

    Change RecycleView structure and add a load_game method SelectableLabel is changed for SelectableButton, to simplify the game selector.

--- amphibian-alchemists/app/classes/gameselector.py
+++ amphibian-alchemists/app/classes/gameselector.py
@@ -1,52 +1,31 @@
 import os
 
 from kivy.app import App
-from kivy.properties import BooleanProperty
+from kivy.properties import StringProperty
 from kivy.storage.jsonstore import JsonStore
-from kivy.uix.behaviors import FocusBehavior
-from kivy.uix.label import Label
-from kivy.uix.recycleboxlayout import RecycleBoxLayout
-from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.screenmanager import Screen
+from kivy.uix.togglebutton import ToggleButton
 
 DATA_DIR = os.path.join(
     App.get_running_app().APP_DIR, os.path.normcase("data/gamestate.json")
 )
 
 
-class SelectableRecycleBoxLayout(
-    FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout
-):
-    """ Adds selection and focus behaviour to the view. """
-
-
-class SelectableLabel(RecycleDataViewBehavior, Label):
-    """ Add selection support to the Label """
-
-    index = None
-    selected = BooleanProperty(False)
-    selectable = BooleanProperty(True)
-
-    def refresh_view_attrs(self, rv, index, data):
-        """ Catch and handle the view changes """
-        self.index = index
-        self.text = data["value"]
-        return super(SelectableLabel, self).refresh_view_attrs(rv, index, data)
-
-    def on_touch_down(self, touch):
-        """ Add selection on touch down """
-        if super(SelectableLabel, self).on_touch_down(touch):
-            return True
-        if self.collide_point(*touch.pos) and self.selectable:
-            return self.parent.select_with_touch(self.index, touch)
-
-    def apply_selection(self, rv, index, is_selected):
-        """ Respond to the selection of items in the view. """
-        pass
+class SelectableButton(RecycleDataViewBehavior, ToggleButton):
+    def on_state(self, widget, value):
+        game_selector_screen = App.get_running_app().root.get_screen(
+            "game_selector_screen"
+        )
+        if value == "down":
+            game_selector_screen.last_selected = widget.text
+        else:
+            game_selector_screen.last_selected = ""
 
 
 class GameSelectorScreen(Screen):
+    last_selected = StringProperty("")
+
     def on_enter(self, *args):
         if not os.path.exists(DATA_DIR):
             store = JsonStore(DATA_DIR)
@@ -62,10 +41,15 @@ class GameSelectorScreen(Screen):
                 title = game["game_title"]
                 created = str(game["created_date"])
                 last_saved = str(game["last_saved_date"])
-                self.ids.rv.data.append(
+                self.rv.data.append(
                     {
-                        "value": f"{title}\n"
+                        "text": f"{title}\n"
                         f"Created: {created}\n"
                         f"Last Saved: {last_saved}"
                     }
                 )
+        else:
+            self.rv.data = [{"text": "No saved games detected."}]
+
+    def load_game(self, game):
+        self.manager.current = "game_screen"
