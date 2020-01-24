5ba3603ac079bc51b2dfb0fdd0a77261e5db0da8
Kylan Byrd <AvianAnalyst@gmail.com>
2020-01-24 05:12:23 -0800

    Added operation button assets, nearly completed ticket having trouble with image size vs button size

--- wandering-warriors/main.py
+++ wandering-warriors/main.py
@@ -27,7 +27,8 @@ class Ledger(LedgerLayout):
 
 
 class OperationsBar(Widget):
-    pass
+    def buttonImage(self, operation: str) -> str:
+        return f'assets/graphics/{operation}.png'
 
 
 class CuneiformDrawingInput(DrawPad):
@@ -35,10 +36,6 @@ class CuneiformDrawingInput(DrawPad):
 
 
 class TopRightButton(Widget):
-    pass
-
-
-class OperationButton(Widget):
     pass
 
 
