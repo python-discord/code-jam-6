fa51178a2d0f517fe7d7117cc1cf005341da5222
chill <karnabalaj@gmail.com>
2020-01-24 23:25:03 +0530

    Change Restpect attr to Welfare

--- litigious-liberators/profile_cards/profile_cards.py
+++ litigious-liberators/profile_cards/profile_cards.py
@@ -1,17 +1,50 @@
+from kivymd.toast import toast
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
 from kivy.config import Config
 from kivy.app import App
-from kivy.clock import Clock
 import os
 from os import listdir
 from random import shuffle
 from copy import deepcopy
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.uix.popup import Popup
+from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
 from yaml import safe_load
 from kivy.core.window import Window
 from collections import Counter
 
 Config.set("input", "mouse", "mouse,multitouch_on_demand")
+
+
+class ScrollableLabel(ScrollView):
+    text = StringProperty("")
+
+
+class Content(BoxLayout):
+    def __init__(self, **kwargs):
+        super(Content, self).__init__(**kwargs)
+        self.orientation = "vertical"
+        self.story = ScrollableLabel()
+        self.button = Button(text="Close")
+        self.add_widget(self.story)
+        self.add_widget(self.button)
+
+
+class ImageButton(ButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        super(ImageButton, self).__init__(**kwargs)
+        self.popup = Popup(content=Content(), size_hint=(0.4, 0.7))
+        self.popup.content.button.bind(on_press=self.popup.dismiss)
+
+    def on_press(self):
+        self.popup.open()
+
+    def close_popup(self):
+        self.popup.dismiss()
 
 
 class SelectionScreen(Screen):
@@ -20,17 +53,6 @@ class SelectionScreen(Screen):
 
 class LossScreen(Screen):
     pass
-
-
-class SwipePopup(Popup):
-    def __init__(self, **kwargs):
-        super(SwipePopup, self).__init__(**kwargs)
-        # call dismiss_popup in 2 seconds
-        self.orientation = "vertical"
-        Clock.schedule_once(self.dismiss_popup, 2)
-
-    def dismiss_popup(self, dt):
-        self.dismiss()
 
 
 class WinScreen(Screen):
@@ -42,18 +64,12 @@ class ProfileCard(Screen):
         super(ProfileCard, self).__init__(**kwargs)
         pic_addr = f"../profiles/pictures/{profile['Picture']}"
         self.ids.picture.source = os.path.join(os.path.dirname(os.path.abspath(__file__)), pic_addr)
-        about_text = f"I'm a {profile['Age']} year old {profile['Species']}"
-        self.right_delta = Counter(profile["right_delta"])
-        self.left_delta = Counter(profile["left_delta"])
-        self.ids.r_a1.text += str(profile["right_delta"]["a1"])
-        self.ids.r_a2.text += str(profile["right_delta"]["a2"])
-        self.ids.r_a3.text += str(profile["right_delta"]["a3"])
-        self.ids.l_a1.text += str(profile["left_delta"]["a1"])
-        self.ids.l_a2.text += str(profile["left_delta"]["a2"])
-        self.ids.l_a3.text += str(profile["left_delta"]["a3"])
-        self.ids.name.text += profile["Name"]
-        self.ids.about.text = about_text
-        self.ids.job.text += profile["What I do"]
+        self.deltas = Counter(profile["Deltas"])
+        self.ids.name.text = profile["Name"]
+        self.ids.right_choice.text += profile["Choices"]["right"]
+        self.ids.left_choice.text += profile["Choices"]["left"]
+        self.results = profile["Results"]
+        self.ids.picture.popup.content.story.text = profile["Biography"]
         self.ids.name.font_name = "../fonts/Oldenburg/Oldenburg-Regular.ttf"
 
 
@@ -67,7 +83,7 @@ class ProfileList(ScreenManager):
         self.profile_list = deepcopy(self.static_profile_list)
         self.cycler = self.r_cycle(self.profile_list)
         #  should be initialisable in main app
-        self._attributes = Counter({"a1": 1, "a2": 1, "a3": 1})
+        self._attributes = Counter({"Knowledge": 5, "Welfare": 5, "Energy": 5})
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         with open(f"{self.profile_dir}/{next(self.cycler)}", "r") as profile_file:
@@ -105,10 +121,15 @@ class ProfileList(ScreenManager):
         totals = Counter()
         totals.update(self.attributes)
         next_card = ProfileCard(profile)
-        delta = current.right_delta if direction == "right" else current.left_delta
+        delta = current.deltas[direction]
         totals.update(delta)
         self.attributes = totals
+        message = direction.upper() + "\n" + current.results[direction]
+        for k, v in delta.items():
+            message += f"{k}: {v if v < 0 else '+' + str(v)}\n"
+        toast(message.rstrip())
         self.switch_to(next_card, direction=direction, transition=trans)
+        current.ids.picture.close_popup()
         self.remove_widget(current)
 
     def _keyboard_closed(self):
@@ -117,13 +138,9 @@ class ProfileList(ScreenManager):
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         if keycode[1] == "right":
-            #  popup = SwipePopup(size_hint=(0.2, 0.4))
-            #  popup.open()
             self.card_swipe("right")
 
         elif keycode[1] == "left":
-            #  popup = SwipePopup(size_hint=(0.2, 0.4))
-            #  popup.open()
             self.card_swipe("left")
         return True
 
