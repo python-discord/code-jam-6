798cfc0d8d40c50e4044ff944039c00a980e3d2f
CharlieADavies <charlieadavies@gmail.com>
2020-01-24 15:38:43 +0000

    Moved widget inheritance to kvlang instead of Python

--- kaleidoscopic-kings/frontend/frontend.py
+++ kaleidoscopic-kings/frontend/frontend.py
@@ -1,30 +1,5 @@
-from kivy.properties import StringProperty
-
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 
 
 class MainWidget(GridLayout):
-    pass
-
-
-class StatusBar(GridLayout):
-    pass
-
-
-class CardWidget(BoxLayout):
-    """
-    Widget that contains a card, art, and buttons to make the relevant choices
-    """
-
-    pass
-
-
-class CardBoxLayout(BoxLayout):
-    """Standard box layout with a stone tablet as a background"""
-
-    source = StringProperty()
-
-
-class StatusWidget(BoxLayout):
-    pass
+    """Parent widget for all children"""
