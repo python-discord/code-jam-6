9f32b99f385f663054aa63f12d4d654ad7ad8773
salt-die <naclmolecule@gmail.com>
2020-01-24 10:35:59 -0600

    dummy save/load placeholders

--- circumstantial-companions/stone.py
+++ circumstantial-companions/stone.py
@@ -216,6 +216,12 @@ class Chisel(Widget):
         self.canvas.clear()
         self.setup_canvas()
 
+    def save(self, path_to_file):
+        pass
+
+    def load(self, path_to_file):
+        pass
+
     def set_radius(self, value):
         self.chisel_radius = value
 
