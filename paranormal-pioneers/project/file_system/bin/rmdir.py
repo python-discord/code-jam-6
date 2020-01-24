c64a0f7a4d9c98f3534dd3f8147673dbea06fcb6
Tansc <loh55kit@gmail.com>
2020-01-24 21:12:36 +0800

    introduce rmdir

---
+++ paranormal-pioneers/project/file_system/bin/rmdir.py
@@ -0,0 +1,32 @@
+from argparse import Namespace
+from pathlib import Path
+import os
+
+from project.core import command
+from project.core.parser import Parser
+from project.core.terminal import Terminal
+from project.core.utils import PathLike, FS
+
+
+class Rmdir(command.Command):
+    """Remove the DIRECTORY(ies), if they are empty."""
+    def __init__(self) -> None:
+        super().__init__(name='rmdir')
+
+    def _resolve_path(self, cwd: PathLike, path: PathLike, filesystem: FS):
+        """Make user provided path relative with current working directory."""
+        path = filesystem.find_dir(cwd, path)
+        filesystem.check_env(path)
+        return path
+
+    @command.option('dir', nargs='+')
+    def handle_dir(self, ns: Namespace, term: Terminal) -> None:
+        self.dirs = [self._resolve_path(term.path, dir, term.fs) for dir in ns.dir]
+
+    def main(self, ns: Namespace, term: Terminal) -> str:
+        for dir in self.dirs:
+            os.rmdir(dir)
+
+
+def setup(parser: Parser) -> None:
+    parser.add_command(Rmdir())
