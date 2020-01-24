0f16027d92ec598daae2fef74ecb1b8477737375
Franccisco <35090042+Franccisco@users.noreply.github.com>
2020-01-23 21:53:39 -0300

    Remove test file

--- amphibian-alchemists/app/test_action.py
+++
@@ -1,4 +0,0 @@
-long_list = ["LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST", "LONGLIST"]
-
-from incorrect import position
-from testingflake import noqatest # noqa
