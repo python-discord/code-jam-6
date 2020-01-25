f39b84f34ce8a608d5cf63a88f35d457731d01fc
Alexander <amaroudis@gmail.com>
2020-01-24 22:20:37 -0500

    Added rotation of cannon

--- next-door-nuns/Cannon.py
+++ next-door-nuns/Cannon.py
@@ -3,14 +3,14 @@ from kivy.properties import Clock
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle
-from kivy.graphics import Rotate
+from kivy.graphics import Rotate, Color,context_instructions
 from kivy.uix.image import Image
 from kivy.properties import NumericProperty
 from math import sin,cos,radians
 from kivy.animation import Animation
 
 class Cannon(Widget):
-    angle = NumericProperty(30)
+    angle_int_deg= NumericProperty(50)
     def __init__(self, **kwargs):
         super(Cannon, self).__init__(**kwargs)
         self.velocity = 0
@@ -18,7 +18,15 @@ class Cannon(Widget):
         with self.canvas:
             self.sky = Rectangle(size=(2000, 1000), source="Back_Drop.png")
             self.grass = Rectangle(size=(2000, 100), source="Grass.png")
-            self.cannon = Rectangle(size=(50, 15), pos=(20, 15), source="Cannon.png")
+
+            # self.cannon = Rectangle(size=(50, 15), pos=(20, 15), source ="cannon.png")
+            # self.canvas.before
+            # context_instructions.PushMatrix()
+            # Rotate(origin=(0, 0), angle=self.angle_int_deg)
+            # Color(rgb=(0, 200, 0))
+            # self.canvas.after
+            # context_instructions.PopMatrix()
+
             self.wheel = Rectangle(size=(35, 35), pos=(20, 0), source="Wheel.png")
             self.cannonball = Rectangle(pos=(1000, 1000), size=(10, 10), source="Cannon_Ball.png")
 
@@ -29,10 +37,11 @@ class Cannon(Widget):
                 print(self.velocity)
 
     def set_angle(self, angle):
-        self.angle_int_deg = int(angle)
+        # self.angle_int_deg = int(angle)
         if int(angle):
             if -1 < int(angle) <= 90:
                 self.angle = int(angle)
+                self.angle_int_deg = self.angle
 
     def rot_animation(self, instance):
         self.animation = Animation(angle=self.angle, duration=1)
@@ -40,10 +49,12 @@ class Cannon(Widget):
         self.animation.start(self.cannon)
 
     def fire_cannon(self):
-        self.cannonball.pos = (self.cannon.pos[0] + self.cannon.size[0], self.cannon.pos[1] + self.cannon.size[1]/2)
+        # self.cannonball.pos = (self.cannon.pos[0] + self.cannon.size[0], self.cannon.pos[1] + self.cannon.size[1]/2)
+        self.cannonball.pos = (35, 20)
         self.velocity_clock = Clock.schedule_interval(self.drop, 0.1)
-        self.velocity_y = self.velocity
-        self.velocity_x = self.velocity
+        self.velocity_y = self.velocity * (self.angle / 90)
+        self.velocity_x = self.velocity - self.velocity_y
+
     def drop(self, dt):
 
         if self.velocity_x >= 0:
