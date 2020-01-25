b617b7c6a903995f9733b06f5fc7b47f8a37a7c2
Tansc <loh55kit@gmail.com>
2020-01-26 00:55:26 +0800

    fix mv command

--- paranormal-pioneers/project/file_system/bin/mv.py
+++ paranormal-pioneers/project/file_system/bin/mv.py
@@ -7,7 +7,7 @@ from project.core.utils import OSException
 
 
 class MV(command.Command):
-    """Move a file or a directory to another directory."""
+    """Move or rename a file/directory."""
     def __init__(self) -> None:
         super().__init__(name='mv')
 
@@ -23,12 +23,9 @@ class MV(command.Command):
         if ns.dest is None:
             raise OSException('error: destination not given')
 
-        self.dest = term.fs.get_path(term.path, ns.dest)
+        self.dest = term.fs.get_path(term.path, ns.dest, check_existing=False)
 
     def main(self, ns: Namespace, term: Terminal) -> None:
-        if self.src.is_file():
-            self.dest /= self.src.name
-
         self.src.rename(self.dest)
 
 
