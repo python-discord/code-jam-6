6136de2271cda75742d4f773b797523f57791eee
Ryan Gables <regables@gmail.com>
2020-01-25 13:40:52 -0800

    record gestures for cunneiform 1 and 10

---
+++ wandering-warriors/modules/gesture_db.py
@@ -0,0 +1,49 @@
+from kivy.gesture import GestureDatabase
+
+gdb = GestureDatabase()
+
+cun_1 = gdb.str_to_gesture(
+    "eNp91FtMFFcYwPFFF4QtSrGoWG+0Ul1aXS9V630XtY6K1lWhDirg7jIyK7LLtzsLrPKFNSpGoQ1tNCo2RZPGWDWR+NCEF/EFE3kQrZcmplqrJsY"
+    "+VKMxxsSkPWfynXgyITMvO/nP78w5czYzqaGh2nBD0lOjxY1ETHMp9AuOor2QhjDEraY7HI7GcLWhw1DFe+H0RXdtv0PNYFHXwjW6AU7F+7zS"
+    "/8eRuU/UHFbpTlX1sWh1ImRAuuJzfpf"
+    "+647nr9Vh7HLciEVrtThkVMCwQafeZAIXZLIFZCG43OZc9dFwxIjDBxWQ7R50mJ8DFwxno0Yg5LjVNDaqCT5Uek+uBK2yJdMMSchVvH87S44"
+    "/O5efCOpcj0T4yK2/dzoT91+9LfjlxWESeQij7MVohDFWcW902dOXxWtJ5COMtYq78y9d9P7oIvExwjh7MR5hglXczPWEzpwNkpiIMMleFCB8Yi8"
+    "+RZhsFVff6Nennb9FohDhM3sxBWGqvXAjFJmiw8yPueju+b7zdOoBic8RvrAX0xCm2wsPwgxTtOU0tD/qv8DFiXP5wy"
+    "//VkBiJsIsezEb4UsuvM3lm7uuOW8w0XuppTDv59zZJOYgzLUX8xC+shfzERaY4ge2X4V5nVz0Bv69"
+    "+VO5SmIhwiJTHLmTnfHX1ilc9JnbK1a6GGGJvViK4DXF8RV8n2Jc9Ne97n6jD5DwIRSboouvzlPCxcDU/eyFOUtiGcJyq"
+    "/j9aPJdx8ZGEisQvrYXKxEUq7htvi9rSKxCWG0Vd9P5Qv4hsQahxCruje3j85BYi7DOXnyDsN4q/sw8NutWqScR1NRs/r0KxTQtQp8ffwVscCu"
+    "+A90Ofii+9g7zpBU2yjFFcZMcfRRLpdj2n3m0Qpkct5P8VoqHxfDNUjw0iYarUjx4imS5FFsdFLew+JDivocUt7L4QsQCitvYKD"
+    "/FvX6KFSymKKbEOitZ7KDYYg5Pa4UqFk9RRLGk7SxeodjsIxmQ4h5xz6AUd6dIhqSYFPeslqODpCbFJvF37JBio3j2GjleoajLsYs2OSxHcc"
+    "+dchSLr5WjeMxdchSbXCfFBjF7RI5i9qgcxSbXSzHRQhLkKDYkJkWjimRcinGxIYYchUzIUSy"
+    "+gcUb1tg4mGxisdsak1oiGFCz2LkR3aXFApGQBrsVb88JfnSqTnYhEqjTYI/KPTQngp7/ARKJ1wE= "
+)
+
+cun_10 = gdb.str_to_gesture(
+    "eNp9lHtQlFUUwBdByIUEs4dlKQraYglkpgTafqb5SRmhaG0QLbCsLCG7e/YBLHnjuctLiD8am"
+    "+jFODXDOOXgOE45OrPQ2MSMk4AMZekkf2hNDwend1Mzde7tHIZhmN1/9u7v+917zj3nfNsYbauqrAmkV9i9Pr"
+    "/HbtTpGwxpTRAlYIHJstBgMNRWlvscEK2br5+d+CN+wwJLLEKHvbLC4YMY3TydljCYMpFtSURKJ1ndHle53"
+    "+aDhbqWlJtRtCT7jCUOH3t9HleV3QuxxRA3b+gCJRjhFkxgkQCjScVyuyqdPi/EF0OCad5t+VIwwq24a7GARJMlCnfVQZJuHn"
+    "+r6K9vdrYqEIAlunnqMv5umfSXOaR9m4ClJgd653t6l4Wu/+hA42pV3vp/ppPJuF3AHcr49MLPG1cMHJHGlRt9E10N2WTcKeAuZYQLwfVdwCSNr"
+    "/t/2/H3geNkLBNwtzIGY1edPrTuJ2l8Obx272e74sm4R8DyyMa9Au6TRvgdmca7+6QxYfk"
+    "+qt1ZTMYKASsjG8kCVinjZM7IWP3RJmmMO63GMzFHyFgtIEUZ4QapqNte2O75c"
+    "/+lNWSkClijjBEsUsq2b6Ux0jK59EbfKBlrBdwf2TAJSFPGmKx0iiaN4X/Vh4x1Ah5QxoRsy81j0vi48dzmjGABGQ8KWK"
+    "+Myfqj4mShWxonZFtywmSkC8j438h97cTF6MXSeFNdq5eMTAEPKWP8fdn+a2iEe776MOujXzeRsUHAw8oYUm1JksbQ8tc"
+    "/OT6cSsZGAY9ENjYJ2BzZyBLwaGQjW0COMgKY3S9nB6RxXq6648jYImBrZOMxAebIhiZgm5rCU7b3cA6rpTEuSzrCnXtcwPbIxg4BTyhjRF1gtTS"
+    "+ePuaY+ueLWTsFKBHNnYJyFXGKA6v1XhKGpd+6Pj988pzZDwp4KnIxm4BTyvjorzgsVRpXMb5KW7eTUaegGeUMamVYSYfSONKWCaS5y+zWxLk"
+    "/5XNY7c76e8nvxj2mHSt56ZBfnTt1SS1CMFehGNzYQHCfrmMmtkSgn24dpNJW0KwH9cGho0En9W17kGG"
+    "+QSf07XDYfWa4FM2LbrWNUSB6GkInte1zkba3sWBCnWtvZdgZwnBIl1r6yfYMUTwBV0LzUD1HRWCYl0LThFs5"
+    "+gvItQYJlJ0q661cpXaOHoJQj6zrZfOLEVYwpBTKkOYzDCTTJuutXD0NgOZ5Qi5SqExMu0I8xlyngcQZjLkQBW61syNC3HjHAg5z"
+    "+BVulElQs4zOEjmSwj5zGADmVUIkxhy4w4i5BZTuUJQrWtNXKVgMkEnQr5mkFNyIRybC92zIfcIEA7NhR6EXKVWHkXvbDhF0DcbjtKN"
+    "/LPOnIE1eCPuO7U1BLUIeepaXWTWYeV55lt4PgNYcB7FJjdtr8dh49I1mMl8GWd1mmb+EHfzEM4/T0ighK4p8E3hlGr5Rq/gK2ml7TXc9wZd6"
+    "+VAft7eiDCLoI+3NyHkvvuslFIzwjqGXOQWhHwjL1+zFSHPkpeTDyIcYMhnhhAOzoVts6FGgdrnMztmn8mBOu3+slLLIlz7XAftnlKnzQ5duvn0G"
+    "/LTZ4nBB87Sajsctkgfuv1l6f8BOux3yA== "
+)
+
+
+def load_gestures():
+    gdb = GestureDatabase()
+    gdb.add_gesture(cun_1)
+    gdb.add_gesture(cun_10)
+    print('Loading gestures...')
+
+    return gdb
