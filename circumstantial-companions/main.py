20104d1383b45f7fdce49798e201d91f158e8aa9
salt-die <naclmolecule@gmail.com>
2020-01-24 20:57:11 -0600

    Animation replaced sliders, some punctuation added to comments, slider logic removed

--- circumstantial-companions/main.py
+++ circumstantial-companions/main.py
@@ -5,8 +5,10 @@ simulation! A caveman workout routine guaranteed to give you chiseled slabs fast
 """
 import contextvars
 import math
+from itertools import cycle
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.metrics import dp, sp
 from kivy.properties import StringProperty
@@ -19,25 +21,22 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.slider import Slider
 from kivy.garden.navigationdrawer import NavigationDrawer
 
 from cursor import Cursor
 from i18n import DEFAULT_LOCALE, SYSTEM_LOCALE, LOCALES, TRANSLATIONS
 from mixins import RepeatingBackground, SignBorder
-from stone import Chisel, CHISEL_RADIUS_RANGE, CHISEL_POWER_RANGE
+from stone import Chisel
 
 font = contextvars.ContextVar("font")
 
 OPTIONS_BACKGROUND = "assets/img/options_background.png"
 HAMMER_ICON = "assets/img/cursor/hammer_up_pixelized.png"
-
+CAVEMAN = tuple(f"assets/img/caveman{i}.png" for i in range(1,5))
 
 class Button(SignBorder, KivyButton):
     def __init__(self, text, **kwargs):
-        super().__init__(
-            text=text, font_name=font.get(), **kwargs,
-        )
+        super().__init__(text=text, font_name=font.get(), **kwargs)
         self.setup_border()
 
 
@@ -45,9 +44,7 @@ class SelectLanguagePopup(SignBorder, Popup):
     choice = StringProperty()
 
     def __init__(self):
-        layout = BoxLayout(
-            orientation="vertical", spacing=dp(34), padding=(dp(20), dp(15))
-        )
+        layout = BoxLayout(orientation="vertical", spacing=dp(34), padding=(dp(20), dp(15)))
         for locale_code, locale_info in LOCALES.items():
             btn = Button(locale_info["name"], font_size=sp(16))
 
@@ -61,60 +58,24 @@ class SelectLanguagePopup(SignBorder, Popup):
             btn.bind(on_release=_make_select_function(locale_code))
             layout.add_widget(btn)
 
-        super().__init__(
-            title=_("Select language"),
-            title_font=font.get(),
-            title_size=sp(20),
-            title_align="center",
-            content=layout,
-            separator_color=(0, 0, 0, 0),
-            size_hint=(0.5, 0.8),
-        )
+        super().__init__(title=_("Select language"),
+                         title_font=font.get(),
+                         title_size=sp(20),
+                         title_align="center",
+                         content=layout,
+                         separator_color=(0, 0, 0, 0),
+                         size_hint=(0.5, 0.8))
         self.setup_border()
-
-
-class ChiselRadiusSlider(Slider):
-    def __init__(self, chisel, **kwargs):
-        self.chisel = chisel
-        super().__init__(
-            orientation="vertical",
-            range=CHISEL_RADIUS_RANGE,
-            value=chisel.chisel_radius,
-            value_track=True,
-            value_track_color=[1, 0, 1, 1],
-            **kwargs,
-        )
-
-    def on_value(self, instance, value):
-        self.chisel.set_radius(value)
-
-
-class ChiselPowerSlider(Slider):
-    def __init__(self, chisel, **kwargs):
-        self.chisel = chisel
-        super().__init__(
-            orientation="vertical",
-            range=CHISEL_POWER_RANGE,
-            value=chisel.chisel_power,
-            value_track=True,
-            value_track_color=[1, 0, 1, 1],
-            **kwargs,
-        )
-
-    def on_value(self, instance, value):
-        self.chisel.set_power(value)
 
 
 class OptionsPanel(RepeatingBackground, BoxLayout):
     def __init__(self, chisel):
         self.chisel = chisel
         self.popup = None
-        super().__init__(
-            orientation="vertical",
-            spacing=dp(34),
-            padding=(dp(20), dp(15)),
-            opacity=0,  # opacity is set when side panel is opened
-        )
+        super().__init__(orientation="vertical",
+                         spacing=dp(34),
+                         padding=(dp(20), dp(15)),
+                         opacity=0)  # opacity is set when side panel is opened
         self.setup_background(OPTIONS_BACKGROUND)
 
     def build(self, locale=SYSTEM_LOCALE):
@@ -128,49 +89,58 @@ class OptionsPanel(RepeatingBackground, BoxLayout):
         font.set(LOCALES[locale]["font"])
 
         # Title
-        title = Label(
-            text=_("Options"),
-            font_name=font.get(),
-            font_size=sp(30),
-            size_hint=(1, 0.05),
-        )
+        title = Label(text=_("Options"),
+                      font_name=font.get(),
+                      font_size=sp(30),
+                      size_hint=(1, 0.05))
 
         # Language selection
         self.popup = SelectLanguagePopup()
-        popup_btn = Button(
-            _("Select language"), font_size=sp(18), size_hint=(1, None), height=dp(42)
-        )
+        popup_btn = Button(_("Select language"),
+                           font_size=sp(18),
+                           size_hint=(1, None),
+                           height=dp(42))
         popup_btn.bind(on_release=self.popup.open)
         self.popup.bind(choice=lambda instance, choice: self.build(choice))
 
         # Reset
-        reset_btn = Button(
-            _("Reset"), font_size=sp(18), size_hint=(1, None), height=dp(42)
-        )
+        reset_btn = Button(_("Reset"),
+                           font_size=sp(18),
+                           size_hint=(1, None),
+                           height=dp(42))
         reset_btn.bind(on_release=lambda btn: self.chisel.reset())
 
         # Source code
-        src_btn = Button(
-            _("Source code"), font_size=sp(18), size_hint=(1, None), height=dp(42)
-        )
+        src_btn = Button(_("Source code"),
+                         font_size=sp(18),
+                         size_hint=(1, None),
+                         height=dp(42))
 
-        # Sliders
-        slider_layout = GridLayout(cols=2, spacing=10, size_hint=(1, 0.7))
-        slider_layout.add_widget(ChiselRadiusSlider(self.chisel, size_hint=(0.5, 0.9)))
-        slider_layout.add_widget(ChiselPowerSlider(self.chisel, size_hint=(0.5, 0.9)))
-        slider_layout.add_widget(Image(source=HAMMER_ICON, size_hint=(0.5, 0.1)))
-        slider_layout.add_widget(Image(source=HAMMER_ICON, size_hint=(0.5, 0.1)))
+        # Animation
+        images = []
+        for source in CAVEMAN:
+            image = Image(source=source, size_hint=(1, 1), allow_stretch=True)
+            image.texture.mag_filter = 'nearest'
+            images.append(image)
+        images = cycle(images)
+
+        animation = Image(source=CAVEMAN[0], size_hint=(1, 1), allow_stretch=True)
+        animation.texture.mag_filter = 'nearest'
+
+        def next_texture(dt):
+            animation.texture = next(images).texture
+        Clock.schedule_interval(next_texture, .2)
 
         self.add_widget(title)
         self.add_widget(popup_btn)
         self.add_widget(reset_btn)
         self.add_widget(src_btn)
-        self.add_widget(slider_layout)
+        self.add_widget(animation)
 
     def update_background(self, instance, value):
-        # Overriden to snap to the right position
+        # Overriden to snap to the right position.
         self.bg_rect.texture.uvsize = self._get_uvsize()
-        self.bg_rect.texture = self.bg_rect.texture  # required to trigger update
+        self.bg_rect.texture = self.bg_rect.texture  # Required to trigger update.
         bg_width, bg_height = self._get_background_size()
         self.bg_rect.pos = (self.right - bg_width, self.y)
         self.bg_rect.size = (bg_width, bg_height)
@@ -191,7 +161,7 @@ class ChiselApp(App):
         navdrawer.add_widget(options_panel)
 
         rel_layout = RelativeLayout()
-        rel_layout.add_widget(chisel)  # to push it when side panel is opened
+        rel_layout.add_widget(chisel)  # To push it when side panel is opened.
         navdrawer.add_widget(rel_layout)
         options_panel.build()
         navdrawer.bind(_anim_progress=self._set_side_panel_opacity)
