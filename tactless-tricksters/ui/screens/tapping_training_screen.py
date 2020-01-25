f9e1f4efa591559d9e88133e38b9b31ef75d3404
kkawabat <kakwabat@asu.edu>
2020-01-24 17:55:18 -0700

    random tests

--- tactless-tricksters/ui/screens/tapping_training_screen.py
+++ tactless-tricksters/ui/screens/tapping_training_screen.py
@@ -1,4 +1,5 @@
 # Kivy imports
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.app import App
@@ -24,53 +25,15 @@ class TappingScreen(Screen):
         self.training_difficulty = ''
 
     def ui_layout(self):
-        record_button_anchor = AnchorLayout(anchor_x='center', anchor_y='bottom',
-                                            padding=[dp(25), dp(25), dp(25), dp(25)])
-
-        self.record_button = MDFloatingActionButton(icon='record', size=[dp(56), dp(56)])
-        self.record_button.md_bg_color = App.get_running_app().theme_cls.primary_color
-        self.record_button.text_color = [1, 1, 1, 1]
-        self.record_button.bind(on_press=lambda x: self.decode_audio())
-        record_button_anchor.add_widget(self.record_button)
-
-        self.decode_input = MDTextFieldRound(pos_hint={'center_x': 0.5, 'center_y': 0.5}, size_hint=(0.85, 0.5))
-        self.decode_input.icon_left_dasabled = True
+        self.decode_input = self.ids['decode_input']
         # Moves widget out of the field of view
         self.decode_input.children[2].children[2].pos_hint = {'center_x': 500, 'center_y': 500}
-        # This binds the right icon to record the input
-        self.decode_input.icon_right = 'database-export'
-        self.decode_input.children[2].children[0].bind(on_press=lambda x: self.clear_text())
 
-        decode_card = MDCard(padding=dp(24), spacing=dp(24), orientation='vertical',
-                             size_hint_x=0.85, size_hint_y=0.7, pos_hint={'top': 0.85, 'center_x': 0.5})
-        decode_label = MDLabel(text='Decode Morse Code Audio', font_style='Body1', halign='center',
-                               size_hint=(1, 0.5))
-        decode_label.theme_text_color = 'Custom'
-        decode_label.text_color = [1, 1, 1, 1]
-        decode_card.add_widget(decode_label)
-
-        self.decode_output_label = MDLabel(text='Hit record or enter Morse Code below to start decoding',
-                                           font_style='Body1', halign='center', size_hint=(1, 0.5))
-        self.decode_output_label.theme_text_color = 'Custom'
-        self.decode_output_label.text_color = [1, 1, 1, 1]
         print(self.amr.bits_per_frame)
-        self.audio_indicator = AudioIndicator(stack_width=self.amr.bits_per_frame)
-        self.audio_indicator.size_hint = (1, 2)
-
-        decode_card.add_widget(self.audio_indicator)
-        decode_card.add_widget(self.decode_output_label)
-        decode_card.add_widget(self.decode_input)
-        decode_card.md_bg_color = App.get_running_app().theme_cls.accent_color
-        decode_card.elevation = 15
-
-        self.add_widget(decode_card)
-        self.add_widget(record_button_anchor)
 
     def update_audio_indicator(self, dt):
         if hasattr(self.audio_indicator, 'stack_width'):
             level_array = [self.audio_indicator.stack_height + 1 if bit == 0 else 0 for bit in dt[-self.audio_indicator.stack_width:]]
-            # for columns in range(self.audio_indicator.stack_width):
-            #     level_array.append(random.randrange(0, self.audio_indicator.stack_height))
             self.audio_indicator.set_levels(level_array)
 
     def clear_text(self):
