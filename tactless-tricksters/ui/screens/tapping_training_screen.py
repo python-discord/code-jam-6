d708dce817983d25c272bba0565cf6a50e10ca9b
kkawabat <kakwabat@asu.edu>
2020-01-24 22:04:03 -0700

    added scrolling background to welcome screen

--- tactless-tricksters/ui/screens/tapping_training_screen.py
+++ tactless-tricksters/ui/screens/tapping_training_screen.py
@@ -1,42 +1,31 @@
 # Kivy imports
-from kivy.properties import ObjectProperty
-from kivy.uix.screenmanager import Screen
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.app import App
-from kivy.metrics import dp
 from kivy.clock import Clock
-
-# kivymd imports
-from kivymd.button import MDFloatingActionButton
-from kivymd.cards import MDCard
-from kivymd.label import MDLabel
-from kivymd.textfields import MDTextFieldRound
-
-# project imports
-from ui.widgets.audio_indicator import AudioIndicator
+from kivy.properties import StringProperty, ObjectProperty
+from kivy.uix.screenmanager import Screen
 
 
 class TappingScreen(Screen):
+    training_difficulty = StringProperty("")
+    decode_input = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super(TappingScreen, self).__init__(name=kwargs.get('name'))
         self.util = kwargs.get('util')
         self.amr = self.util.auto_morse_recognizer
-        self.ui_layout()
-        self.training_difficulty = ''
-
-    def ui_layout(self):
-        self.decode_input = self.ids['decode_input']
-        # Moves widget out of the field of view
-        self.decode_input.children[2].children[2].pos_hint = {'center_x': 500, 'center_y': 500}
-
-        print(self.amr.bits_per_frame)
+        self.decode_output_label = self.ids.decode_output_label
+        self.record_button = self.ids.record_button
+        self.audio_indicator = self.ids.audio_indicator
 
     def update_audio_indicator(self, dt):
         if hasattr(self.audio_indicator, 'stack_width'):
             level_array = [self.audio_indicator.stack_height + 1 if bit == 0 else 0 for bit in dt[-self.audio_indicator.stack_width:]]
             self.audio_indicator.set_levels(level_array)
 
-    def clear_text(self):
+    def update_text(self, morse_code):
+        self.decode_input.text = self.decode_input.text + ''.join(morse_code)
+
+    def clear_input(self):
         self.decode_input.text = ''
 
     def decode_audio(self):
@@ -53,16 +42,13 @@ class TappingScreen(Screen):
             self.record_button.md_bg_color = App.get_running_app().theme_cls.primary_color
             Clock.unschedule(self.update_amr)
             self.amr.stop()
-            self.clear_text()
+            self.clear_input()
 
     def update_amr(self, kargs):
         print(kargs)
         morse_code, bit_signal = self.amr.update()
         self.update_audio_indicator(bit_signal)
         self.update_text(morse_code)
-
-    def update_text(self, morse_code):
-        self.decode_input.text = self.decode_input.text + ''.join(morse_code)
 
     def return_home(self):
         self.manager.current = 'welcome'
--- tactless-tricksters/ui/screens/welcome_screen.py
+++ tactless-tricksters/ui/screens/welcome_screen.py
@@ -1,20 +1,34 @@
-from kivy.uix.screenmanager import Screen
+from kivy.app import App
 from kivy.clock import Clock
+from kivy.properties import ListProperty, ObjectProperty
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.screenmanager import Screen
 from kivymd.button import MDRectangleFlatIconButton
 from kivymd.label import MDLabel
+from kivymd.toolbar import MDToolbar
 
 
 class WelcomeScreen(Screen):
+    texture = ObjectProperty(None)
+    tex_coords = ListProperty([0, 0, 1, 0, 1, 1, 0, 1])
+
     def __init__(self, **kwargs):
         super(WelcomeScreen, self).__init__(name=kwargs.get('name'))
+        Clock.schedule_once(self.texture_init, 0)
+
+        Clock.schedule_interval(self.scroll_texture, 1/60.)
         self.util = kwargs.get('util')
         self.app = App.get_running_app()
         self.ui_layout()
 
-    def ui_layout(self):
-        with self.canvas:
-            Rectangle(source="ui/img/welcome_2.png", size=(Window.width, Window.height))
+    def texture_init(self, *args):
+        self.canvas.before.children[-1].texture.wrap = 'repeat'
 
+    def scroll_texture(self, dt):
+        for i in range(0, 8, 2):
+            self.tex_coords[i] += dt / 3.
+
+    def ui_layout(self):
         toolbar_anchor = AnchorLayout(anchor_x='center', anchor_y='top')
         toolbar = MDToolbar(title="Enigma", anchor_title='center')
         toolbar.md_bg_color = self.app.get_running_app().theme_cls.primary_color
@@ -23,7 +37,6 @@ class WelcomeScreen(Screen):
         welcome_label = MDLabel(text='Welcome!', font_style='H4', halign='center')
         welcome_label.theme_text_color = 'Custom'
         welcome_label.text_color = [1, 1, 1, 1]
-
 
 
 class WelcomeButton(MDRectangleFlatIconButton):
@@ -41,10 +54,3 @@ class WelcomeButton(MDRectangleFlatIconButton):
         # Set Icon to white
         self.children[0].children[1].text_color = [1, 1, 1, 1]
         self.children[0].children[1].font_size = 30
-
-
-class WelcomeScreen(Screen):
-
-    def __init__(self, **kwargs):
-        super(WelcomeScreen, self).__init__(name=kwargs.get('name'))
-        self.util = kwargs.get('util')
