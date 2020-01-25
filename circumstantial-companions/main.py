dbcc86d828fa3ee0b52bdfb4475a924c8388341f
MusicOnline <39178127+MusicOnline@users.noreply.github.com>
2020-01-25 18:48:40 +0800

    Implement import and save as

--- circumstantial-companions/main.py
+++ circumstantial-companions/main.py
@@ -5,6 +5,7 @@ simulation! A caveman workout routine guaranteed to give you chiseled slabs fast
 """
 import contextvars
 import math
+import os
 from itertools import cycle
 
 from kivy.app import App
@@ -15,12 +16,14 @@ from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button as KivyButton
 from kivy.uix.dropdown import DropDown
+from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.textinput import TextInput
 from kivy.garden.navigationdrawer import NavigationDrawer
 
 from cursor import Cursor
@@ -34,6 +37,8 @@ CAVEMAN = tuple(f"assets/img/caveman{i}.png" for i in range(4))
 BUTTON_NORMAL = "assets/img/button_normal.png"
 BUTTON_HOVER = "assets/img/button_hover.png"
 BUTTON_PRESSED = "assets/img/button_pressed.png"
+FILE_EXTENSION = ".chisel-project"
+
 
 class Button(SignBorder, KivyButton):
     def __init__(self, text, **kwargs):
@@ -78,10 +83,122 @@ class SelectLanguagePopup(SignBorder, Popup):
         self.setup_border()
 
 
+class ImportPopup(SignBorder, Popup):
+    def __init__(self, chisel):
+        self.chisel = chisel
+        layout = BoxLayout(orientation="vertical", spacing=dp(34), padding=(dp(20), dp(15)))
+        self.file_chooser = FileChooserListView(path=".", filters=[self._filter_file], size_hint=(1, 0.85))
+        self.btn = Button(_("Open..."), disabled=True, font_size=sp(16), size_hint=(1, 0.15))
+
+        self.file_chooser.bind(path=self._change_title, selection=self._change_btn_name)
+        self.btn.bind(on_release=self._select_file)
+
+        layout.add_widget(self.file_chooser)
+        layout.add_widget(self.btn)
+
+        super().__init__(title="",
+                         title_font=FONT.get(),
+                         title_size=sp(20),
+                         title_align="center",
+                         content=layout,
+                         separator_color=(0, 0, 0, 0),
+                         size_hint=(0.7, 0.9))
+        self._change_title()
+        self.setup_border()
+
+    @staticmethod
+    def _filter_file(folder, filename):
+        return filename.endswith(FILE_EXTENSION)
+
+    def _change_title(self, *args):
+        path = self.file_chooser.path
+        if path == ".":
+            path = os.getcwd()
+        self.title = _("Import from {path}").format(path=path)
+
+    def _change_btn_name(self, *args):
+        selection = self.file_chooser.selection
+        if selection:
+            self.btn.text = _('Open "{filename}"').format(filename=os.path.basename(selection[0]))
+            self.btn.disabled = False
+        else:
+            self.btn.text = _("Please select a file.")
+            self.btn.disabled = True
+
+    def _select_file(self, *args):
+        selection = self.file_chooser.selection
+        if selection:
+            self.dismiss()
+            self.chisel.load(selection[0])
+
+    def on_dismiss(self, *args):
+        self.file_chooser.cancel()
+
+
+class SaveAsPopup(SignBorder, Popup):
+    def __init__(self, chisel):
+        self.chisel = chisel
+        layout = BoxLayout(orientation="vertical", spacing=dp(34), padding=(dp(20), dp(15)))
+        self.file_chooser = FileChooserListView(path=".", filters=[self._filter_file], size_hint=(1, 0.75))
+        self.text_input = TextInput(text="Untitled" + FILE_EXTENSION, multiline=False, font_name=FONT.get(), font_size=sp(16), size_hint=(1, 0.1))
+        self.btn = Button("", font_size=sp(16), size_hint=(1, 0.15))
+        self._change_btn_name()
+
+        self.file_chooser.bind(path=self._change_title, selection=self._set_text)
+        self.text_input.bind(text=self._change_btn_name, on_text_validate=self._save_file)
+        self.btn.bind(on_release=self._save_file)
+
+        layout.add_widget(self.file_chooser)
+        layout.add_widget(self.text_input)
+        layout.add_widget(self.btn)
+
+        super().__init__(title="",
+                         title_font=FONT.get(),
+                         title_size=sp(20),
+                         title_align="center",
+                         content=layout,
+                         separator_color=(0, 0, 0, 0),
+                         size_hint=(0.7, 0.9))
+        self._change_title()
+        self.setup_border()
+
+    @staticmethod
+    def _filter_file(folder, filename):
+        return filename.endswith(FILE_EXTENSION)
+
+    @staticmethod
+    def _resolve_filename(string):
+        if string.endswith(FILE_EXTENSION):
+            return string
+        return string + FILE_EXTENSION
+
+    def _change_title(self, *args):
+        path = self.file_chooser.path
+        if path == ".":
+            path = os.getcwd()
+        self.title = _("Save to {path}").format(path=path)
+
+    def _set_text(self, *args):
+        selection = self.file_chooser.selection
+        if selection:
+            self.text_input.text = os.path.basename(selection[0])
+
+    def _change_btn_name(self, *args):
+        filename = self._resolve_filename(self.text_input.text)
+        self.btn.text = _('Save as "{filename}"').format(filename=filename)
+
+    def _save_file(self, *args):
+        filename = self._resolve_filename(self.text_input.text)
+        self.chisel.save(os.path.join(self.file_chooser.path, filename))
+        self.dismiss()
+
+    def on_dismiss(self, *args):
+        self.file_chooser.cancel()
+
+
 class OptionsPanel(RepeatingBackground, BoxLayout):
     def __init__(self, chisel):
         self.chisel = chisel
-        self.popup = None
         super().__init__(orientation="vertical",
                          spacing=dp(32),
                          padding=(dp(20), dp(15)),
@@ -105,25 +222,25 @@ class OptionsPanel(RepeatingBackground, BoxLayout):
                       size_hint=(1, 0.05))
 
         # Language selection
-        self.popup = SelectLanguagePopup()
-        popup_btn = Button(_("Select language"),
+        language_btn = Button(_("Select language"),
                            font_size=sp(18),
                            size_hint=(1, None),
                            height=dp(42))
-        popup_btn.bind(on_release=self.popup.open)
-        self.popup.bind(choice=lambda instance, choice: self.build(choice))
+        language_btn.bind(on_release=self.open_language_popup)
 
         # Import
         import_btn = Button(_("Import..."),
                          font_size=sp(18),
                          size_hint=(1, None),
                          height=dp(42))
+        import_btn.bind(on_release=lambda btn: ImportPopup(self.chisel).open(btn))
 
-        # Export
-        export_btn = Button(_("Export..."),
+        # Save as
+        save_as_btn = Button(_("Save as..."),
                          font_size=sp(18),
                          size_hint=(1, None),
                          height=dp(42))
+        save_as_btn.bind(on_release=lambda btn: SaveAsPopup(self.chisel).open(btn))
 
         # Reset
         reset_btn = Button(_("Reset"),
@@ -156,9 +273,9 @@ class OptionsPanel(RepeatingBackground, BoxLayout):
 
         widgets = [
             title,
-            popup_btn,
+            language_btn,
             import_btn,
-            export_btn,
+            save_as_btn,
             reset_btn,
             src_btn,
             animation
@@ -174,6 +291,11 @@ class OptionsPanel(RepeatingBackground, BoxLayout):
         bg_width, bg_height = self._get_background_size()
         self.bg_rect.pos = (self.right - bg_width, self.y)
         self.bg_rect.size = (bg_width, bg_height)
+
+    def open_language_popup(self, btn):
+        language_popup = SelectLanguagePopup()
+        language_popup.bind(choice=lambda instance, choice: self.build(choice))
+        language_popup.open(btn)
 
 
 class ChiselApp(App):
