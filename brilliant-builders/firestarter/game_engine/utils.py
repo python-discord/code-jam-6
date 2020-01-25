23c358b0df672ab2ee933690df25d53bda681289
Akarys42 <Akarys42@users.noreply.github.com>
2020-01-25 11:18:29 +0100

    Break get_all_subclasses into 3 lines

--- brilliant-builders/firestarter/game_engine/utils.py
+++ brilliant-builders/firestarter/game_engine/utils.py
@@ -3,4 +3,7 @@ from typing import Set
 
 def get_all_subclasses(cls: type) -> Set[type]:
     """Return all subclasses of a give cls class."""
-    return set(cls.__subclasses__()).union([s for c in cls.__subclasses__() for s in get_all_subclasses(c)])
+    return set(
+        cls.__subclasses__()).union(
+            [s for c in cls.__subclasses__() for s in get_all_subclasses(c)]
+        )
