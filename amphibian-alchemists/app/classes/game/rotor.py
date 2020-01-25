305e153d78261beb815804fa5b13504087d0eefb
Franccisco <35090042+Franccisco@users.noreply.github.com>
2020-01-25 02:16:29 -0300

    Add a gear movement when a rotor is adjusted

--- amphibian-alchemists/app/classes/game/rotor.py
+++ amphibian-alchemists/app/classes/game/rotor.py
@@ -10,3 +10,18 @@ class RotorScreen(Screen):
             self.rotor_section.ids.second_rotor.rotor_value.text,
             self.rotor_section.ids.third_rotor.rotor_value.text,
         )
+
+    def handle_rotor_value(self, instance, rotor_value):
+        gear = self.rotor_section.children[
+            self.rotor_section.children.index(instance.parent.parent) - 1
+        ]
+        if instance.text == "+":
+            rotor_value.text = self.keys[::-1][
+                self.keys[::-1].index(rotor_value.text) - 1
+            ]
+            gear.index = (gear.index + 1) if gear.index < 9 else 0
+            gear.source = f"misc/gear{gear.index}.png"
+        elif instance.text == "-":
+            rotor_value.text = self.keys[self.keys.index(rotor_value.text) - 1]
+            gear.index = (gear.index - 1) if gear.index > 0 else 9
+            gear.source = f"misc/gear{gear.index}.png"
