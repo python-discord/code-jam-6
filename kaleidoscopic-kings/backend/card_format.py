92f5c4f238c5da5cec6774be3e8b3bc0be00b94f
Braindead <albertopoljak@gmail.com>
2020-01-25 18:25:14 +0100

    Removed finished TODO comment

--- kaleidoscopic-kings/backend/card_format.py
+++ kaleidoscopic-kings/backend/card_format.py
@@ -26,7 +26,6 @@ class GameVariable:
     For example if the _INTEGER_RANGE_INCLUDING is (0, 1000) then that means that states of
     type int, when updated, will never go over 1000 or below 0.
     """
-    # TODO implement limits
     INTEGER_RANGE_INCLUDING: ClassVar[Tuple[int, int]] = (0, 1000)
     FLOAT_RANGE_INCLUDING: ClassVar[Tuple[float, float]] = (0.0, 1.0)
 
