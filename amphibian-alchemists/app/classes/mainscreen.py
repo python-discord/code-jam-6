547c45bd38594fe247545c3745291ee15eb1c85f
alex t <sloopooicecream@gmail.com>
2020-01-24 23:42:48 -0500

    made menu music stop play methods

--- amphibian-alchemists/app/classes/mainscreen.py
+++ amphibian-alchemists/app/classes/mainscreen.py
@@ -7,4 +7,10 @@ class MainScreen(Screen):
         super().__init__(**kwargs)
         self.music = SoundLoader.load("misc/SneakySnooper.mp3")
         self.music.loop = True
+        self.start_menu_music()
+
+    def start_menu_music(self):
         self.music.play()
+
+    def stop_menu_music(self):
+        self.music.stop()
