50a065f46aed0fcff9f4eca1f22021c5feffb4d5
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2020-01-25 22:49:35 +0100

    Add set alpha to health bar

--- microscopic-monks/primal/gui/health.py
+++ microscopic-monks/primal/gui/health.py
@@ -1,4 +1,6 @@
-from kivy.graphics.instructions import RenderContext
+from typing import Union
+
+from kivy.graphics.instructions import RenderContext, InstructionGroup
 
 from primal.engine.sprite import ColorSprite
 
@@ -9,11 +11,8 @@ class HealthBar:
         self.health = max_health
 
         self.size = size
-
-
         self.bg = ColorSprite(None, pos, size, (.0, .0, .0, .25))
         self.bar = ColorSprite(None, pos, size, (1, .0, .0))
-
 
     def get_current_width(self) -> float:
         if self.health < 0:
@@ -23,10 +22,17 @@ class HealthBar:
 
         return self.health * (self.size[0] / self.max_health)
 
+    def get_health(self) -> float:
+        return self.health
+
     def set_health(self, health: float):
         self.health = health
         self.bar.set_size((self.get_current_width(), self.size[1]))
 
-    def draw(self, canvas: RenderContext):
+    def draw(self, canvas: Union[InstructionGroup, RenderContext]):
         self.bg.draw(canvas)
         self.bar.draw(canvas)
+
+    def set_alpha(self, alpha: float):
+        self.bg.set_alpha(alpha)
+        self.bar.set_alpha(alpha)
