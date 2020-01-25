d1e4f17c8d0b9c5081346517fe8f95bba12615a3
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2020-01-25 17:11:10 +0100

    Add health bar

---
+++ microscopic-monks/primal/gui/health.py
@@ -0,0 +1,30 @@
+from kivy.graphics.instructions import RenderContext
+
+from primal.engine.sprite import ColorSprite
+
+
+class HealthBar:
+    def __init__(self, pos, size, max_health: float = 100):
+        self.max_health = max_health
+        self.health = max_health
+
+        self.size = size
+
+        self.bg = ColorSprite(None, pos, size, (0.427, .0, .0))
+        self.bar = ColorSprite(None, pos, size, (1, 0, 0))
+
+    def get_current_width(self) -> float:
+        if self.health < 0:
+            return 0.0
+        if self.health > self.max_health:
+            return self.size[1]
+
+        return self.health * (self.size[0] / self.max_health)
+
+    def set_health(self, health: float):
+        self.health = health
+        self.bar.set_size((self.get_current_width(), self.size[1]))
+
+    def draw(self, canvas: RenderContext):
+        self.bg.draw(canvas)
+        self.bar.draw(canvas)
--- microscopic-monks/primal/screens/game_screen.py
+++ microscopic-monks/primal/screens/game_screen.py
@@ -3,13 +3,14 @@ from primal.engine.camera import OrthographicCamera
 from primal.engine.sprite import Player
 from primal.engine.world import World
 
+from primal.gui.health import HealthBar
+
 
 class GameScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.camera = OrthographicCamera(self.canvas, 1280, 720)
-
         self.camera.start_region()
 
         self.world = World((0, 0))
@@ -17,7 +18,18 @@ class GameScreen(Screen):
 
         self.player = Player('testimg.png', (0, 0), (100, 100), 0)
         self.player.draw(self.canvas)
+
         self.camera.end_region()
+
+        # render gui
+        self.gui_camera = OrthographicCamera(self.canvas, 1280, 720)
+        self.gui_camera.start_region()
+
+        self.health_bar = HealthBar((20, 680), (250, 20), 100.0)
+        self.health_bar.draw(self.canvas)
+        self.health_bar.set_health(80)
+
+        self.gui_camera.end_region()
 
     def update(self, delta: float):
         # Maybe move it to player update?
