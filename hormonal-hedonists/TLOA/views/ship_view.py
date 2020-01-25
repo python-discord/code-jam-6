19bcde91e60ca67978e9c801283240731043fcdc
Pippi <tienlocnguyen.ac@gmail.com>
2020-01-25 21:41:57 +0700

    Add burning effect on ship

--- hormonal-hedonists/TLOA/views/ship_view.py
+++ hormonal-hedonists/TLOA/views/ship_view.py
@@ -1,6 +1,6 @@
 import math
 
-from TLOA.core.constants import ATLAS_PATH
+from TLOA.core.constants import ATLAS_PATH, IMAGES_PATH, LIGHT_FOCUS_OFFSET
 from TLOA.entities import BrownShip
 
 from kivy.uix.image import Image
@@ -8,6 +8,9 @@ from kivy.uix.widget import Widget
 
 HP_BAR_X_OFFSET = 75
 HP_BAR_Y_OFFSET = 150
+
+FIRE_X_OFFSET = 75
+FIRE_Y_OFFSET = 20
 
 
 class ShipView(Widget):
@@ -21,9 +24,12 @@ class ShipView(Widget):
 
         self._ship_image = Image(source=source, pos=self.pos)
         self._ship_image.size = self._ship_image.texture_size
+        self._fire = Image(source=IMAGES_PATH.format('fire.zip'), anim_delay=0.1)
+        self._fire.opacity = 0
 
         self.add_widget(self._hp_bar)
         self.add_widget(self._ship_image)
+        self.add_widget(self._fire)
 
         ship.bind(health=self.on_health_change)
         self.bind(pos=self.on_pos_changed)
@@ -32,10 +38,12 @@ class ShipView(Widget):
         health = math.ceil(value / 10) * 10
         self._hp_bar.source = ATLAS_PATH.format(health)
         # burn the ship if health < 50
-
+        if health <=80:
+            self._fire.opacity = 100
         # destroy it if health is 0
 
     def on_pos_changed(self, _ship_view, pos):
         x, y = self._ship_image.pos = pos
         self._hp_bar.pos = (x + HP_BAR_X_OFFSET, y + HP_BAR_Y_OFFSET)
+        self._fire.pos = (x + FIRE_X_OFFSET, y + FIRE_Y_OFFSET)
         # TODO for burning effect
