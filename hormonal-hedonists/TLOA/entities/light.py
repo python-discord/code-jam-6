a559a7c312c6b0d7e9efef3426d582ac5acb379f
sandhan26 <sandhan.sarma26@gmail.com>
2020-01-25 13:11:54 +0530

    Switched to hsv mode for light rays.

--- hormonal-hedonists/TLOA/entities/light.py
+++ hormonal-hedonists/TLOA/entities/light.py
@@ -1,6 +1,8 @@
+from TLOA.core.constants import (
+    LIGHT_COLOR_MAX_HUE, LIGHT_COLOR_MAX_SATURATION,
+    LIGHT_COLOR_MAX_VALUE, LIGHT_COLOR_MAX_ALPHA)
 from kivy.graphics import Mesh, Color
 from kivy.vector import Vector
-
 from typing import List
 
 
@@ -8,7 +10,12 @@ class LightRays(Mesh):
 
     def __init__(self, point: Vector, surface: List[Vector], **kwargs):
         super(LightRays, self).__init__(**kwargs)
-        self.color = Color(01.0, 0.98, 0.1, 0.65)
+        #self.color = Color(01.0, 0.98, 0.1, 0.65)
+        self.color = Color(h=LIGHT_COLOR_MAX_HUE,
+                           s=LIGHT_COLOR_MAX_SATURATION,
+                           v=LIGHT_COLOR_MAX_VALUE,
+                           a=LIGHT_COLOR_MAX_ALPHA,
+                           mode='hsv')
         self.vertices = []
         self.indices = []
         self.mode = 'traingle_fan'
