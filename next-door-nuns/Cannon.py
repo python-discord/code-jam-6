0d95eacc8e2cf1100291ece2aeabfed71ef1c444
Alexander <amaroudis@gmail.com>
2020-01-25 03:02:24 -0500

    Changed rotation of cannon from Image to Canvas/Rectangle.  Added Python Discord logo for target

--- next-door-nuns/Cannon.py
+++ next-door-nuns/Cannon.py
@@ -14,20 +14,22 @@ class Cannon(Widget):
     def __init__(self, **kwargs):
         super(Cannon, self).__init__(**kwargs)
         self.velocity = 0
-        self.angle = 30
+        self.angle = 0
         with self.canvas:
             self.sky = Rectangle(size=(2000, 1000), source="Back_Drop.png")
             self.grass = Rectangle(size=(2000, 100), source="Grass.png")
 
-            # self.cannon = Rectangle(size=(50, 15), pos=(20, 15), source ="cannon.png")
-            # self.canvas.before
-            # context_instructions.PushMatrix()
-            # Rotate(origin=(0, 0), angle=self.angle_int_deg)
-            # Color(rgb=(0, 200, 0))
-            # self.canvas.after
-            # context_instructions.PopMatrix()
-
             self.wheel = Rectangle(size=(35, 35), pos=(20, 0), source="Wheel.png")
+
+            self.canvas.before
+            context_instructions.PushMatrix()
+            self.rotate = Rotate(origin=(35,12.5), angle=self.angle)
+            self.cannon = Rectangle(size=(50, 15), pos=(35, 10), source="cannon.png")
+            #self.cannon = Rectangle(size=(50, 15), pos=(35, 15))
+            context_instructions.PopMatrix()
+            self.canvas.after
+
+
             self.cannonball = Rectangle(pos=(1000, 1000), size=(10, 10), source="Cannon_Ball.png")
 
     def set_velocity(self, velocity):
@@ -41,7 +43,9 @@ class Cannon(Widget):
         if int(angle):
             if -1 < int(angle) <= 90:
                 self.angle = int(angle)
-                self.angle_int_deg = self.angle
+                self.rotate.angle = self.angle
+
+
 
     def rot_animation(self, instance):
         self.animation = Animation(angle=self.angle, duration=1)
@@ -50,7 +54,9 @@ class Cannon(Widget):
 
     def fire_cannon(self):
         # self.cannonball.pos = (self.cannon.pos[0] + self.cannon.size[0], self.cannon.pos[1] + self.cannon.size[1]/2)
-        self.cannonball.pos = (35, 20)
+        self.cannonball.pos = (int(self.cannon.pos[0]+cos(radians(self.angle))*self.cannon.size[0]),
+                               int(self.cannon.pos[1] + sin(radians(self.angle))*self.cannon.size[1]))
+        print(self.cannonball.pos)
         self.velocity_clock = Clock.schedule_interval(self.drop, 0.1)
         self.velocity_y = self.velocity * (self.angle / 90)
         self.velocity_x = self.velocity - self.velocity_y
@@ -60,13 +66,16 @@ class Cannon(Widget):
         if self.velocity_x >= 0:
             self.cannonball.pos = (self.cannonball.pos[0] + self.velocity_x, self.cannonball.pos[1] + self.velocity_y)
             self.velocity_y -= 0.98
+            
+
         else:
             Clock.unschedule(self.velocity_clock)
         if self.cannonball.pos[1] <= 0:
             self.cannonball.pos = (self.cannonball.pos[0], 0)
             Clock.unschedule(self.velocity_clock)
 
-
+class Target(Widget):
+    pass
 class CannonApp(App):
     def build(self):
         return kv
