470831529bd35bee303b655a07e0763b10ea2c24
Franccisco <35090042+Franccisco@users.noreply.github.com>
2020-01-26 00:13:57 -0300

    Remove autosave option and fix the way the fullscreen setting is handled

--- amphibian-alchemists/app/classes/settingsscreen.py
+++ amphibian-alchemists/app/classes/settingsscreen.py
@@ -23,25 +23,22 @@ class SettingsScreen(Screen):
             Config.write()
 
         for config_name in self.ids.keys():
+            if config_name == "allow_fullscreen":
+                self.ids.allow_fullscreen.state = (
+                    "down" if Config.get("graphics", "fullscreen") == "1" else "normal"
+                )
+                continue
             if not self.store.exists(config_name):
                 self.store.put(config_name, value=1)
             self.set_config_value(config_name)
-        self.ids.allow_fullscreen.state = (
-            "down" if Config.get("graphics", "fullscreen") == "1" else "normal"
-        )
 
     def set_config_value(self, config_name):
         value_to_set = {
-            "auto_save": "down"
-            if self.store.get(config_name)["value"] == 1
-            else "normal",
             "background_volume": self.store.get(config_name)["value"],
             "effects_volume": self.store.get(config_name)["value"],
         }.get(config_name)
 
-        if value_to_set in {"down", "normal"}:
-            self.ids[config_name].state = value_to_set
-        elif type(value_to_set) in {float, int} and (
+        if type(value_to_set) in {float, int} and (
             value_to_set >= 0 or value_to_set <= 1
         ):
             self.ids[config_name].value_normalized = value_to_set
@@ -49,18 +46,19 @@ class SettingsScreen(Screen):
     def save_settings(self, to_save):
         new_value = {
             "allow_fullscreen": 1 if self.ids.allow_fullscreen.state == "down" else 0,
-            "auto_save": 1 if self.ids.auto_save.state == "down" else 0,
             "background_volume": self.ids.background_volume.value_normalized,
             "effects_volume": self.ids.effects_volume.value_normalized,
         }.get(to_save)
 
         if type(new_value) in {int, float} and (new_value >= 0 or new_value <= 1):
-            self.store.put(to_save, value=new_value)
             if to_save == "background_volume":
                 self.manager.get_screen("main_screen").music.volume = new_value
             elif to_save == "allow_fullscreen":
                 Config.set("graphics", "fullscreen", new_value)
                 Config.write()
+                return
+
+            self.store.put(to_save, value=new_value)
 
     def delete_saved_games(self, popup):
         JsonStore(DATA_DIR).clear()
