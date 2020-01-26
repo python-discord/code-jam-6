73eafd04c2b35d2ccb2731260b05265a7e649dcb
CharlieADavies <charlieadavies@gmail.com>
2020-01-26 01:19:47 +0000

    Added even for on_card_change

--- kaleidoscopic-kings/frontend/frontend.py
+++ kaleidoscopic-kings/frontend/frontend.py
@@ -1,5 +1,19 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
 
 class MainWidget(BoxLayout):
     """Parent widget for all children"""
+
+
+class ChoiceButton(Button):
+    def __init__(self, **kwargs):
+        super(Button, self).__init__(**kwargs)
+        self.register_event_type('on_card_change')
+
+    def on_press(self):
+        super().on_press()
+        self.dispatch('on_card_change')
+
+    def on_card_change(self):
+        print('on_card_change')
