8ca2e69e0ca2f4009b646c817cbbe322633f304d
CharlieADavies <charlieadavies@gmail.com>
2020-01-26 13:00:49 +0000

    fixed bugs

--- kaleidoscopic-kings/frontend/frontend.py
+++ kaleidoscopic-kings/frontend/frontend.py
@@ -7,13 +7,20 @@ class MainWidget(BoxLayout):
 
 
 class ChoiceButton(Button):
+    """The button which the user clicks to make the choice between two cards"""
+
     def __init__(self, **kwargs):
         super(Button, self).__init__(**kwargs)
         self.register_event_type("on_card_change")
 
     def on_press(self):
+        """
+        Dispatches an event to show when the user has changed the card.
+        This is useful for applying animations
+        """
         super().on_press()
         self.dispatch("on_card_change")
 
     def on_card_change(self):
-        print("on_card_change")
+        """Default handler"""
+        pass
