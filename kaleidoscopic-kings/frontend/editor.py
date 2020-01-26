d00aaeb358dfb7d6ff841b6662589238adf31f0b
Braindead <albertopoljak@gmail.com>
2020-01-26 05:22:51 +0100

    Added a very very crude GUI layout for editor

---
+++ kaleidoscopic-kings/frontend/editor.py
@@ -0,0 +1,31 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class MainWindow(Screen):
+    pass
+
+
+class CardAddWindow(Screen):
+    pass
+
+
+class GameStatesWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("Editor.kv")
+
+
+class EditorApp(App):
+    def build(self):
+        return kv
+
+
+if __name__ == "__main__":
+    EditorApp().run()
