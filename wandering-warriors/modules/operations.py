7ad0b1cb8b9a001c83e57849e2b508f8ae33adea
Kylan Byrd <AvianAnalyst@gmail.com>
2020-01-25 23:51:01 -0800

    Initial connection between ledger and operations complete
    Issues with sizing and placement still need to be sorted out.

--- wandering-warriors/modules/operations.py
+++ wandering-warriors/modules/operations.py
@@ -1,11 +1,11 @@
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
-from .ledger import LedgerLayout
 
 
 class OperationsLayout(Widget):
     def button_image(self, operation: str) -> str:
         return f'assets/graphics/{operation}.png'
 
-    def send_operation(self, operation: Image) -> None:
-        LedgerLayout.add_middle(operation)
+    def send_operation(self, operation: str) -> None:
+        img_source = self.button_image(operation)
+        self.parent.children[2].add_middle(Image(source=img_source))
