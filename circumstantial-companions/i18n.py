c33fd6300485391d4f877bf078ad4d66a33034bb
salt-die <naclmolecule@gmail.com>
2020-01-26 12:52:45 -0600

    fixed shadowing

--- circumstantial-companions/i18n.py
+++ circumstantial-companions/i18n.py
@@ -13,14 +13,14 @@ TRANSLATIONS = {}
 with open("locales.json", encoding="utf-8") as file:
     LOCALES = json.load(file)
 
-for locale in LOCALES:
-    if locale == DEFAULT_LOCALE:
-        TRANSLATIONS[locale] = gettext.NullTranslations()
+for locale_ in LOCALES:
+    if locale_ == DEFAULT_LOCALE:
+        TRANSLATIONS[locale_] = gettext.NullTranslations()
         continue
 
-    path = Path("locales") / locale / "LC_MESSAGES"
+    path = Path("locales") / locale_ / "LC_MESSAGES"
     with (path / "messages.po").open(encoding="utf-8") as file:
         catalog = read_po(file, ignore_obsolete=True)
     with (path / "messages.mo").open("wb") as file:
         write_mo(file, catalog)
-    TRANSLATIONS[locale] = gettext.translation("messages", "locales", [locale])
+    TRANSLATIONS[locale_] = gettext.translation("messages", "locales", [locale_])
