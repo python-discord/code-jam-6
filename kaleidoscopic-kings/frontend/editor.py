73ad5ffc3c6575af95f749d6e3c4b622faed2d51
Braindead <albertopoljak@gmail.com>
2020-01-26 07:34:38 +0100

    Game states now loaded and showed in GUI

--- kaleidoscopic-kings/frontend/editor.py
+++ kaleidoscopic-kings/frontend/editor.py
@@ -1,11 +1,14 @@
+import json
 from backend import path_handler
 
+from kivy.uix.gridlayout import GridLayout
 from kivy.core.audio import SoundLoader
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
 from kivy.uix.image import Image
+from kivy.uix.button import Button
 from kivy.config import Config
 Config.set("graphics", "width", "1000")
 Config.set("graphics", "height", "600")
@@ -22,10 +25,9 @@ class CardAddWindow(Screen):
                   f"Continuing but please make sure to add sound file in that path.")
         else:
             # TODO pressing multiple times plays multiples times ><
-            sound = SoundLoader.load(str(full_sound_path))
-            if sound:
-                print(sound.state)
-                sound.play()
+            sound_file = SoundLoader.load(str(full_sound_path))
+            if sound_file:
+                sound_file.play()
             else:
                 print("Sound file found but could not be played.")
 
@@ -42,8 +44,23 @@ class CardAddWindow(Screen):
 
 
 class GameStatesWindow(Screen):
-    def wtf(self):
-        print("hm??!?!")
+    with open(path_handler.get_game_state_json_path(story_name)) as f:
+        game_states = json.load(f)
+
+    def on_pre_enter(self):
+        layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+
+        for key, value in self.game_states.items():
+            btn = Button(text=f"{key} : {value}")
+            layout.add_widget(btn)
+
+        self.ids.game_states_scroll_list.add_widget(layout)
+
+    def on_pre_leave(self):
+        self.ids.game_states_scroll_list.clear_widgets()
+
+    def wtf(self, root_ids):
+        print(root_ids)
 
 
 class WindowManager(ScreenManager):
