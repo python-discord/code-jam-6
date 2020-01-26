c14a3d5e3c53342cbeca979dcc1db1246f3ff681
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2020-01-26 14:51:18 +0100

    Refactor inventory

--- microscopic-monks/primal/gui/inventory.py
+++ microscopic-monks/primal/gui/inventory.py
@@ -8,34 +8,56 @@ import json
 
 class Inventory:
     def __init__(self, pos):
-
-        # accidentally switched items and grid, pls ignore that mistake
         self.items = []
         self.grid = []
+        self.active = 0
+
         for i in range(10):
-            self.grid.append(Sprite("blank.png", (pos[0] + 5, pos[1] + 60 * i + 5), (40, 40)))
+            self.items.append(Sprite("blank.png", (pos[0] + 5, pos[1] + 60 * i + 5), (40, 40)))
+
         for i in range(10):
-            self.items.append(
-                ColorSprite(None, (pos[0], pos[1] + 60 * i), (50, 50), (.0, .0, .0, .25)))
+            self.grid.append(
+                ColorSprite(None, (pos[0], pos[1] + 60 * i), (50, 50), (1, 1, 1, .25)))
+
+        self.set_ative(self.active)
+
+    def set_ative(self, active: int):
+        self.grid[self.active].set_alpha(.25)
+        self.active = active
+        self.grid[self.active].set_alpha(.8)
 
     def draw(self, canvas: RenderContext):
-        for i in self.items:
-            i.draw(canvas)
         for i in self.grid:
             i.draw(canvas)
+        for i in self.items:
+            i.draw(canvas)
+
+    def load_inventory(self):
+        with open((Sprite.resource_dir / "inventory.json").as_posix(), "r") as read_file:
+            data = json.load(read_file)
+
+        with open((Sprite.resource_dir / "items.json").as_posix(), "r") as read_file:
+            items = json.load(read_file)
+
+        i = 0
+        for item in data:
+            try:
+                self.items[i].set_source(items[item]["source"])
+            except Exception:
+                pass
+            i += 1
 
     def update(self):
         with open((Sprite.resource_dir / "inventory.json").as_posix(), "r") as read_file:
             data = json.load(read_file)
 
         with open((Sprite.resource_dir / "items.json").as_posix(), "r") as read_file:
-            itms = json.load(read_file)
+            items = json.load(read_file)
 
         i = 0
         for item in data:
             try:
-                self.grid[i].set_source(itms[item]["source"])
-                self.grid[i].set_alpha(1)
+                self.items[i].set_source(items[item]["source"])
             except Exception:
                 pass
             i += 1
